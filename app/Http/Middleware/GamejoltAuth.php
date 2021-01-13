1ad45ae8dc1f052dbc7760285143ef6d43e7384d
Daniel S. Billing <daniel@infihex.com>
2021-01-13 20:58:29 +0100

    Added GamejoltAuth middleware, WIP

---
+++ app/Http/Middleware/GamejoltAuth.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class GamejoltAuth
+{    
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!env("GAMEJOLT_GAME_ID") || env("GAMEJOLT_GAME_PRIVATE_KEY")) {
+            redirect()->route('login')->with('error', 'Gamejolt API keys is not set by an admin!');
+        }
+        // TODO: Check if user is logged in or not
+        return $next($request);
+    }
+}

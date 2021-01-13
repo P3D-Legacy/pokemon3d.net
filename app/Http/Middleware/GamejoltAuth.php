bf21e4189349d003579a83036f3734b1265a606f
Daniel S. Billing <daniel@infihex.com>
2021-01-13 22:24:05 +0100

    Update GamejoltAuth.php

--- app/Http/Middleware/GamejoltAuth.php
+++ app/Http/Middleware/GamejoltAuth.php
@@ -17,9 +17,12 @@ class GamejoltAuth
     public function handle(Request $request, Closure $next)
     {
         if (!env("GAMEJOLT_GAME_ID") || !env("GAMEJOLT_GAME_PRIVATE_KEY")) {
-            redirect()->route('login')->with('error', 'Gamejolt API keys is not set by an admin!');
+            redirect()->route('login')->with('error', 'Gamejolt API keys is not set by the admin!');
         }
         // TODO: Check if user is logged in or not
+        if (!$request->session()->get('gjid')){
+            return redirect()->route('login');
+        }
         return $next($request);
     }
 }

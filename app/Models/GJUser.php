3582e2933bcc9642d634fb8d07eb1948f4a6e9d1
Daniel S. Billing <daniel@infihex.com>
2021-01-25 21:20:05 +0100

    Let's log changes to the GJUser

--- app/Models/GJUser.php
+++ app/Models/GJUser.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Models\Activity;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class GJUser extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, LogsActivity;
 
     /**
      * The table associated with the model.
@@ -28,4 +30,18 @@ class GJUser extends Model
         'is_admin',
     ];
 
+    protected static $logAttributes = [
+        'gjid',
+        'gju',
+        'is_admin',
+    ];
+    protected static $logOnlyDirty = true;
+    protected static $submitEmptyLogs = false;
+
+    // Since we are using sessions with gamejolt logins we have to tap the activity to log the causer
+    public function tapActivity(Activity $activity)
+    {
+        $activity->causer_id = $this->where('gjid', session()->get('gjid'))->first()->id;
+        $activity->causer_type = get_class($this);
+    }
 }

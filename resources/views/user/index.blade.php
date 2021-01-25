e256fdb6313bdfdd792d28288f32681d1e6d9d19
Daniel S. Billing <daniel@infihex.com>
2021-01-25 21:02:34 +0100

    User functions for admin #4

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.main')
+@section('title', 'Users')
+     
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <table class="table table-bordered table-responsive my-3">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Game Jolt ID</th>
+                    <th scope="col">Game Jolt Username</th>
+                    <th scope="col">Is Admin</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td>{{ $user->gjid }}</td>
+                        <td>{{ $user->gju }}</td>
+                        <td>{{ ($user->is_admin) ? 'Yes' : 'No' }}</td>
+                        <td><a class="btn btn-warning" href="{{ route('user-edit', $user->id) }}">Edit</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SkinController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ImportController;
 
 /*
@@ -25,3 +26,7 @@ Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/import/{id}', [ImportController::class, 'import'])->name('import');
 Route::post('/skin/create', [SkinController::class, 'store'])->name('skin-store');
 Route::get('/skin/delete', [SkinController::class, 'destroy'])->name('skin-destroy');
+
+Route::get('/users', [UserController::class, 'index'])->name('users');
+Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('user-edit');
+Route::post('/user/edit/{id}', [UserController::class, 'update'])->name('user-update');

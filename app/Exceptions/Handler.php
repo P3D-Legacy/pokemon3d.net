a1f3916dd62f2d2113b031b667b20d32976d6b5e
Daniel S. Billing <daniel@infihex.com>
2021-05-09 13:44:05 +0200

    Installed sentry/sentry-laravel

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -34,7 +34,9 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if ($this->shouldReport($e) && app()->bound('sentry')) {
+                app('sentry')->captureException($e);
+            }
         });
     }
 }

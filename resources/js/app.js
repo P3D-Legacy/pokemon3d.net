f9851ccdca3ee5d0c0be3c8bd986a178b415b1e4
Daniel S. Billing <daniel.billing@intility.no>
2021-06-18 22:54:38 +0200

    Installed Tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 require('./bootstrap');
+
+require('alpinejs');
+
+require('@fortawesome/fontawesome-free/js/all.js');
+
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            backgroundImage: theme => ({
+                'woods': "url('/img/bg.jpg')",
+            }),
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};

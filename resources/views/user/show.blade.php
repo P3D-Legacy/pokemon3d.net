6a038035f883a5b5a7261e603c9229c43fe8b310
Daniel S. Billing <daniel.billing@intility.no>
2021-06-29 21:34:56 +0200

    Updated user profile

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -2,15 +2,13 @@
 @section('title', $user->gju)
      
 @section('content')
-<div class="row">
-    <div class="col-12">
-        <h2>{{ $user->gju }} <small class="text-muted">{{ $user->gjid }}</small></h2>
-    </div>
-</div>
-<hr>
-<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+<h2 class="text-3xl font-extrabold leading-9 border-b-2 border-gray-100 text-gray-50 mb-4 mt-4 pb-1">
+    {{ $user->gju }} <small class="text-muted">{{ $user->gjid }}</small>
+</h2>
+
+<div class="gap-4 grid grid-cols-1 grid-flow-row auto-rows-max sm:grid-cols-2 lg:grid-cols-3">
     @if(!$user->skins->count())
-        <p>None found.</p>
+        <p class="text-white">None found.</p>
     @endif
     @foreach($user->publicSkins as $skin)
         @include('skin.component.card', ['skin' => $skin])

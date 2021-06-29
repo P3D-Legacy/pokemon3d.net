50affb6d0e516f2f2cc397e8be98ec546a7da22d
Daniel S. Billing <daniel.billing@intility.no>
2021-06-29 19:30:19 +0200

    Updated alert styling

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -299,27 +299,27 @@
             </nav>
 --}}
             @if (session('error'))
-                <div class="alert alert-danger">
+                <div class="px-4 py-3 leading-normal text-red-100 bg-red-700 rounded-lg">
                     <i class="fas fa-frown mr-2" aria-hidden="true"></i> {{ session('error') }}
                 </div>
             @endif
             @if (session('success'))
-                <div class="alert alert-success">
+                <div class="px-4 py-3 leading-normal text-green-100 bg-green-700 rounded-lg">
                     <i class="fas fa-check-circle mr-2" aria-hidden="true"></i> {{ session('success') }}
                 </div>
             @endif
             @if (session('info'))
-                <div class="alert alert-info">
+                <div class="px-4 py-3 leading-normal text-blue-100 bg-blue-700 rounded-lg">
                     <i class="fas fa-info-circle mr-2" aria-hidden="true"></i> {{ session('info') }}
                 </div>
             @endif
             @if (session('warning'))
-                <div class="alert alert-warning">
+                <div class="px-4 py-3 leading-normal text-yellow-100 bg-yellow-700 rounded-lg">
                     <i class="fas fa-exclamation mr-2" aria-hidden="true"></i> {{ session('warning') }}
                 </div>
             @endif
             @if ($errors->any())
-                <div class="alert alert-warning my-3" role="alert">
+                <div class="px-4 py-3 leading-normal text-yellow-100 bg-yellow-700 rounded-lg" role="alert">
                     <strong><i class="fas fa-exclamation-triangle mr-2" aria-hidden="true"></i> Input Warning</strong>
                     <ul class="m-0">
                         @foreach ($errors->all() as $error)

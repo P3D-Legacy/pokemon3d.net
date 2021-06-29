d29e4ece996f043027d565ae488a82588ebc5697
Daniel S. Billing <daniel.billing@intility.no>
2021-06-29 18:49:10 +0200

    Updated public skin pages

--- resources/views/skin/public/popular.blade.php
+++ resources/views/skin/public/popular.blade.php
@@ -2,36 +2,26 @@
 @section('title', 'Public Skins')
      
 @section('content')
-<div class="row">
-    <div class="col-12">
-        <h2 class="text-3xl font-extrabold leading-9 border-b-2 border-gray-100 text-gray-900 mb-12">Public Skins</h2>
-    </div>
+<h2 class="text-3xl font-extrabold leading-9 border-b-2 border-gray-100 text-gray-50 mb-4 mt-4 pb-1">
+    Public Skins
+</h2>
+
+<div class="flex items-center mb-4">
+    <a class="border-l border-t border-b text-base font-medium rounded-l-md hover:bg-gray-100 px-4 py-2 {{ request()->is('skins/public/new*') ? 'text-green-800 bg-green-50' : 'text-gray-800' }} bg-white" href="{{ route('skins-newest') }}">Newest</a>
+    <a class="border-t border-b border-r text-base font-medium rounded-r-md hover:bg-gray-100 px-4 py-2 {{ request()->is('skins/public/popular*') ? 'text-green-800 bg-green-50' : 'text-gray-800 bg-white' }}" href="{{ route('skins-popular') }}">Most Popular</a>
 </div>
-<div class="row">
-    <div class="col-12">
-        <ul class="nav nav-tabs mb-3" id="myTab" role="tablist">
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" href="{{ route('skins-newest') }}">Newest</a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link active" href="{{ route('skins-popular') }}">Most Popular</a>
-            </li>
-        </ul>
-        <div class="flex flex-auto">
-            @if(!$skins->count())
-                <p>None found.</p>
-            @endif
-            @foreach($skins as $skin)
-                @include('skin.component.card', ['skin' => $skin])
-            @endforeach
-        </div>
-        
-    </div>
+
+<div class="gap-4 grid grid-flow-row auto-cols-auto md:grid-flow-col md:auto-cols-max">
+    @if(!$skins->count())
+        <p>None found.</p>
+    @endif
+    @foreach($skins as $skin)
+        @include('skin.component.card', ['skin' => $skin])
+    @endforeach
 </div>
-<div class="row mt-3">
-    <div class="col-12">
-        {{ $skins->links() }}
-    </div>
+
+<div class="mt-4">
+    {{ $skins->links() }}
 </div>
 
 @endsection

6e33304d38a5dc9aabbf355c9967b1d9c046640d
Daniel S. Billing <daniel.billing@intility.no>
2021-06-29 17:36:33 +0200

    Updated my skins header

--- resources/views/skin/my.blade.php
+++ resources/views/skin/my.blade.php
@@ -2,12 +2,9 @@
 @section('title', 'My Skins')
      
 @section('content')
-<div class="row">
-    <div class="col-12">
-        <h2>My Skins <a class="btn btn-sm btn-primary" href="{{ route('skin-create') }}">Create</a></h2>
-        <p>Slots: {{ \App\Models\GJUser::find(session()->get('gjid'))->skins()->count() }} / {{ env('SKIN_MAX_UPLOAD') }}</p>
-    </div>
-</div>
+<h2 class="text-3xl font-extrabold leading-9 border-b-2 border-gray-100 text-gray-50 mb-4 mt-4 pb-1">
+    My Skins &mdash; {{ \App\Models\GJUser::find(session()->get('gjid'))->skins()->count() }} / {{ env('SKIN_MAX_UPLOAD') }}
+</h2>
 <div class="gap-4 grid grid-flow-row auto-cols-auto md:grid-flow-col md:auto-cols-max">
     @foreach($skins as $skin)
         @include('skin.component.card', ['skin' => $skin])

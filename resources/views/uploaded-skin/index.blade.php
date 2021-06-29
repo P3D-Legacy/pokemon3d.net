0141e85b8ebe9d50d67c931b6a1ce057082aef18
Daniel S. Billing <daniel.billing@intility.no>
2021-06-29 21:35:37 +0200

    Updated Uploaded Skins

--- resources/views/uploaded-skin/index.blade.php
+++ resources/views/uploaded-skin/index.blade.php
@@ -2,44 +2,14 @@
 @section('title', 'Uploaded Skins')
      
 @section('content')
-<div class="row">
-    <div class="col-12">
-        <h2>Uploaded Skins</h2>
-    </div>
-</div>
-<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+<h2 class="text-3xl font-extrabold leading-9 border-b-2 border-gray-100 text-gray-50 mb-4 mt-4 pb-1">Uploaded Skins</h2>
+
+<div class="gap-4 grid grid-cols-1 grid-flow-row auto-rows-max sm:grid-cols-2 lg:grid-cols-3">
+    @if(!$skins->count())
+        <p class="text-white">None found.</p>
+    @endif
     @foreach($skins as $skin)
-        <div class="col">
-            <div class="card">
-                <div class="row g-0">
-                    <div class="col-4 p-2">
-                        <img src="{{ Storage::disk('skin')->exists($skin->path()) ? asset('skin/'.$skin->path()) : asset('img/noskin.png') }}" height="128" width="96">
-                    </div>
-                    <div class="col-8">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $skin->name }}</h5>
-                            <p class="card-text text-muted">
-                                <small>{{ $skin->uuid }}</small>
-                                <br>
-                                <small>{{ Storage::disk('skin')->exists($skin->path()) ? \ByteUnits\Binary::bytes(Storage::disk('skin')->size($skin->path()))->format() : 'N/A' }}</small>
-                                <br>
-                                <small>Owned by <a href="{{ route('user-show', $skin->owner_id) }}">{{ $skin->user->gju ?? 'Game Jolt ID: '.$skin->owner_id }}</a></small>
-                            </p>
-                            <form class="row row-cols-lg-auto g-3 align-items-center" method="post" action="{{ route('uploaded-skin-destroy', $skin->uuid) }}">
-                                <div class="col-12">
-                                    <input class="form-control form-control-sm" type="text" name="reason" placeholder="Add a legit reason here">
-                                </div>
-                                <div class="col-12">
-                                    <button class="btn btn-sm btn-outline-danger">Delete</button>
-                                </div>
-                                @csrf
-                            </form>
-                            <p class="m-0 text-muted"><small>Users will be able to see the reason!</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('skin.component.card', ['skin' => $skin])
     @endforeach
 </div>
 

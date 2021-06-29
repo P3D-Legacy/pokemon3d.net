8fd663f30452a8d804528a6b2efe65def624dc38
Daniel S. Billing <daniel.billing@intility.no>
2021-06-29 21:42:10 +0200

    Fixed logout link

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -227,7 +227,7 @@
                                                 </button>
                                                 <div x-show="open" @click.outside="open = false" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 border border-1 border-gray-300">
                                                     <div class="py-1 " role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                                                        <a href="#" class="block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
+                                                        <a href="{{ route('logout') }}" class="block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
                                                             <span class="flex flex-col">
                                                                 <span>
                                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">

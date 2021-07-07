028dc241efbebce60107e93b903352a80f49db0d
Daniel S. Billing <daniel.billing@intility.no>
2021-07-07 19:36:59 +0200

    Hide show button on the "show" page :)

--- resources/views/skin/component/card.blade.php
+++ resources/views/skin/component/card.blade.php
@@ -42,13 +42,15 @@
                     </button>
                 </form>
             @else
-                <a class="px-2 py-1 bg-blue-800 text-blue-50 text-xs font-bold uppercase rounded" href="{{ route('skin-show', $skin->uuid) }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                    </svg>
-                    Show
-                </a>
+                @if(!request()->is('skins/public/*'))
+                    <a class="px-2 py-1 bg-blue-800 text-blue-50 text-xs font-bold uppercase rounded" href="{{ route('skin-show', $skin->uuid) }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                        Show
+                    </a>
+                @endif
                 @if(session()->get('gjid') != $skin->owner_id)
                     @if($skin->isLikedBy(\App\Models\GJUser::find(session()->get('gjid'))))
                         <a class="px-2 py-1 bg-red-800 text-red-50 text-xs font-bold uppercase rounded" href="{{ route('skin-like', $skin->uuid) }}">

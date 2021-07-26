94ca1977d95d2d46fced1407d2c135342d142502
Daniel S. Billing <daniel.billing@intility.no>
2021-07-26 21:12:11 +0200

    Updated logo in header

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <x-logo-icon class="block h-8 w-auto" />
                     </a>
                 </div>
 

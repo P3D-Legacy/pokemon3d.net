6ab25b23c0858422be08fc046ecd4a51477c2294
Daniel S. Billing <daniel.billing@intility.no>
2021-07-26 22:01:30 +0200

    Added spring bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            backgroundImage: theme => ({
+                'spring': "url('/img/spring.png')",
+            }),
         },
     },
 

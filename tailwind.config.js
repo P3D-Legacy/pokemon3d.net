5fc9a360c89cda7a6991736bb395d91401cf7ee6
Daniel S. Billing <daniel.billing@intility.no>
2021-07-27 20:19:37 +0200

    Import aspect-ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,5 +26,9 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio')
+    ],
 };

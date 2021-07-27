7f0332daf5529595941d357bc64153ea5c72cd15
Daniel S. Billing <daniel.billing@intility.no>
2021-07-27 22:57:58 +0200

    Added home routes for the rest of the auth pages

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,7 +1,9 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <a href="{{ route('home') }}">
+                <x-jet-authentication-card-logo />
+            </a>
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">

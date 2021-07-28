5f0eb7e692e22322bb3e73c06dc39b0c4b38d6c0
Daniel S. Billing <daniel.billing@intility.no>
2021-07-28 21:26:20 +0200

    Add Consents to user profile

---
+++ app/Http/Livewire/Profile/Consent.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Livewire\Profile;
+
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class Consent extends Component
+{
+    public $consents = [];
+
+    public function mount() {
+        $this->consents = config('app.consents');
+    }
+
+    public function render()
+    {
+        return view('livewire.profile.consent');
+    }
+
+    public function consentGiven($consent) {
+        $user = Auth::user();
+
+        return $user->hasGivenConsent($consent);
+    }
+
+    public function toggleConsent($consent) {
+        $user = Auth::user();
+
+        if ($user->hasGivenConsent($consent)) {
+            return $user->revokeConsentTo($consent);
+        }
+
+        return $user->giveConsentTo($consent, [
+            'text' => $this->consents[$consent],
+        ]);
+
+    }
+}

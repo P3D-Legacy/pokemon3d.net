975f04fe9913415829c4267862ae6d88ad5afac0
Daniel S. Billing <daniel.billing@intility.no>
2021-07-28 21:27:04 +0200

    Added banner for new TOS updates

---
+++ app/Http/Livewire/NewTermsBanner.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+
+class NewTermsBanner extends Component
+{
+    public function agree()
+    {
+        Auth::user()->giveConsentTo(config('app.required_consent'), [
+            'text' => config('app.consents')[config('app.required_consent')],
+        ]);
+        redirect()->route('profile.show');
+    }
+    public function render()
+    {
+        return view('livewire.new-terms-banner');
+    }
+}
---
+++ resources/views/livewire/new-terms-banner.blade.php
@@ -0,0 +1,24 @@
+<div class="fixed z-50 top-0 inset-x-0 pb-2 sm:pb-5">
+    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="p-2 rounded-b-lg bg-green-600/80 shadow-xl sm:p-3">
+            <div class="flex items-center justify-between flex-wrap">
+                <div class="w-0 flex-1 flex items-center">
+                    <span class="flex p-2 rounded-lg bg-green-800">
+                        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
+                        </svg>
+                    </span>
+                    <p class="ml-3 font-medium text-white truncate">
+                        We have updated our <a href="{{ route('terms.show') }}" class="text-white hover:text-white underline">Terms of Service</a>. You must agree to the new Terms of Service before continuing to use the site.
+                    </p>
+                </div>
+                <div class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+                    <button wire:click="agree" class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-green-600 bg-white hover:bg-green-50">
+                        {{ __('I Accept') }}
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="fixed top-0 left-0 h-full w-full bg-black/80 z-40 backdrop-filter backdrop-blur-sm"></div>

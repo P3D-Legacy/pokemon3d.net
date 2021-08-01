9f0212451d1f0aad4a347320a65054078a979175
Daniel S. Billing <daniel.billing@intility.no>
2021-08-01 12:59:34 +0200

    Update email.notifications text

--- config/app.php
+++ config/app.php
@@ -27,7 +27,7 @@ return [
 
     'consents' => [
         'tos.1' => 'Terms of Service &mdash; updated 2021-07-28',
-        'email.notifications' => 'E-mail: Receive alert if you have unread alerts',
+        'email.notifications' => 'E-mail: Receive an e-mail if you have unread notifications',
     ],
 
     /*--------------------------------------------------------------------------

13a752a39006f129681e0875b39477c57c0c8d2b
Daniel S. Billing <daniel.billing@intility.no>
2021-08-06 21:07:01 +0200

    Added validation and verification for username and token

--- app/Http/Livewire/Profile/GameJoltAccount.php
+++ app/Http/Livewire/Profile/GameJoltAccount.php
@@ -2,27 +2,27 @@
 
 namespace App\Http\Livewire\Profile;
 
+use Carbon\Carbon;
 use Livewire\Component;
+use Illuminate\Validation\Rule;
+use Harrk\GameJoltApi\GamejoltApi;
 use Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpFoundation\Request;
+use Harrk\GameJoltApi\GamejoltConfig;
+use Harrk\GameJoltApi\Exceptions\TimeOutException;
 
 class GameJoltAccount extends Component
 {
-    /**
-     * The component's state.
-     *
-     * @var array
-     */
-    public $state = [
-        'gamejolt_username' => '',
-        'gamejolt_token' => '',
-    ];
-
-    public $consents = [];
+    public $gamejolt_username;
+    public $gamejolt_token;
+    public $gamejolt_updated_at;
+    public $gamejolt_verified_at;
 
     public function mount() {
-        $this->state['gamejolt_username'] = Auth::user()->gamejolt_username;
-        $this->state['gamejolt_token'] = Auth::user()->gamejolt_token;
+        $user = Auth::user();
+        $this->gamejolt_username = $user->gamejolt_username;
+        $this->gamejolt_token = $user->gamejolt_token;
+        $this->gamejolt_updated_at =  ($user->gamejolt_updated_at ? $user->gamejolt_updated_at->diffForHumans() : null);
+        $this->gamejolt_verified_at =  ($user->gamejolt_verified_at ? $user->gamejolt_verified_at->diffForHumans() : null);
     }
 
     /**
@@ -30,12 +30,62 @@ class GameJoltAccount extends Component
      *
      * @return void
      */
-    public function updateGameJoltAccount(Request $request)
+    public function save()
     {
         $this->resetErrorBag();
+        $this->resetValidation();
 
-        Auth::user()->update($request->all());
+        $user = Auth::user();
 
+        $this->validate([
+            'gamejolt_username' => [
+                'required',
+                'alpha_dash',
+                'max:30',
+                'min:4',
+                Rule::unique('users')->ignore($user->id),
+            ],
+            'gamejolt_token' => [
+                'required',
+                'alpha_dash',
+                'max:30',
+                'min:4',
+            ],
+        ]);
+
+        $api = new GamejoltApi(new GamejoltConfig(env("GAMEJOLT_GAME_ID"), env("GAMEJOLT_GAME_PRIVATE_KEY")));
+        
+        try {
+            $auth = $api->users()->auth($this->gamejolt_username, $this->gamejolt_token);
+        } catch (TimeOutException $e) {
+            $this->addError('error', $e->getMessage());
+            return false; // Stop here
+        }
+        
+        if(filter_var($auth['response']['success'], FILTER_VALIDATE_BOOLEAN) === false) {
+            $error = $auth['response']['message'];
+            // Better description of username/token error
+            if($error == "No such user with the credentials passed in could be found.") {
+                $error = "Username and/or token is wrong.";
+            }
+            $this->addError('error', $error);
+            return false; // Stop here
+        }
+        
+        $data = [
+            'gamejolt_username' => $this->gamejolt_username,
+            'gamejolt_token' => $this->gamejolt_token,
+            'gamejolt_updated_at' => Carbon::now()->toDateTimeString(),
+            'gamejolt_verified_at' => Carbon::now()->toDateTimeString(),
+        ];
+        
+        $user->update($data);
+        
+        $this->gamejolt_username = $user->gamejolt_username;
+        $this->gamejolt_token = $user->gamejolt_token;
+        $this->gamejolt_updated_at = $user->gamejolt_updated_at->diffForHumans();
+        $this->gamejolt_verified_at = $user->gamejolt_verified_at->diffForHumans();
+        
         $this->emit('saved');
     }
 
--- resources/views/livewire/profile/game-jolt-account.blade.php
+++ resources/views/livewire/profile/game-jolt-account.blade.php
@@ -1,22 +1,23 @@
-<x-jet-form-section submit="updateGameJoltAccount">
+<x-jet-form-section submit="save">
     <x-slot name="title">
         <img src="{{ asset('img/gamejolt-logo-light-1x.png') }}" class="inline-block">
     </x-slot>
 
     <x-slot name="description">
-        {{ __('Link your account with GameJolt to be able to edit your skins for in-game use, and more features to come.') }}
+        <span class="inline-block">{{ __('Link your account with GameJolt to be able to edit your skins for in-game use, and more features to come.') }}</span>
+        <span class="inline-block mt-1">{{ __('Last Updated:') }} {{ $gamejolt_updated_at }} &middot; {{ __('Last Verified:') }} {{ $gamejolt_verified_at }}</span>
     </x-slot>
 
     <x-slot name="form">
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="gamejolt_username" value="{{ __('GameJolt Username') }}" />
-            <x-jet-input id="gamejolt_username" type="text" class="mt-1 block w-full" wire:model.defer="state.gamejolt_username" />
+            <x-jet-label for="gamejolt_username" value="{{ __('Username') }}" />
+            <x-jet-input id="gamejolt_username" type="text" class="mt-1 block w-full" wire:model="gamejolt_username" />
             <x-jet-input-error for="gamejolt_username" class="mt-2" />
         </div>
 
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="gamejolt_token" value="{{ __('GameJolt Token') }}" />
-            <x-jet-input id="gamejolt_token" type="password" class="mt-1 block w-full" wire:model.defer="state.gamejolt_token" />
+            <x-jet-label for="gamejolt_token" value="{{ __('Token') }}" />
+            <x-jet-input id="gamejolt_token" type="password" class="mt-1 block w-full" wire:model="gamejolt_token" />
             <x-jet-input-error for="gamejolt_token" class="mt-2" />
         </div>
     </x-slot>
@@ -25,6 +26,10 @@
         <x-jet-action-message class="mr-3 text-green-500" on="saved">
             {{ __('Saved.') }}
         </x-jet-action-message>
+
+        @error('error')
+            <span class="mr-3 text-red-500">{{ $message }}</span>
+        @enderror
 
         <x-jet-button>
             {{ __('Save') }}

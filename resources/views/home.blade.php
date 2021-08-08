f6c7ac13440569b745497f550e3c6af20297158d
Daniel S. Billing <daniel.billing@intility.no>
2021-08-08 19:41:53 +0200

    Updated responsive menu icon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,9 @@
             
             <div class="block lg:hidden pr-4">
                 <button @click="open = ! open" id="nav-toggle" class="flex items-center px-2 py-1 border rounded text-white border-white hover:text-gray-200 hover:border-gray-200 appearance-none focus:outline-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>

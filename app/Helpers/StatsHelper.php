c0b8cd2d75f7100e9628d17d9726620a85d15111
Daniel S. Billing <daniel.billing@intility.no>
2021-08-08 22:46:21 +0200

    Added StatsHelper that @mrmacgeek made in previous project, with minor tweaks

---
+++ app/Helpers/StatsHelper.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Support\Facades\Http;
+use RestCord\DiscordClient;
+
+class StatsHelper
+{
+
+    const METHOD_GET = 'get';
+    const METHOD_POST = 'post';
+    private $discordClient;
+
+    public function __construct()
+    {
+        $this->discordClient = config('discord.token') ? new DiscordClient(['token' => config('discord.token')]) : null;
+    }
+
+    public function getDiscordServer(){
+        return $this->discordClient->guild->getGuild(['guild.id' => config('discord.server_id'), 'with_counts' => true]);
+    }
+
+    public static function countDiscordMembers(){
+        $that = new StatsHelper;
+        try {
+            return $that->getDiscordServer()->approximate_member_count;
+        }
+        catch(\Exception $exception) {
+            return 'N/A';
+        }
+    }
+
+    public static function countForumMembers(){
+        try{
+            $count = XenforoHelper::getUserCount();
+            return $count;
+        } catch (\Exception $exception){
+            return 'N/A';
+        }
+    }
+
+    public static function countPlayers(){
+        $data = self::sendRequest("/server/status");
+        return count($data['players']);
+    }
+
+    public static function getInGameSeason(){
+        $season = date('W') % 4;
+        $seasonName = "spring";
+        #echo "Season (WOY % 4): " . $season;
+        if($season == 0) {
+            $seasonName = "fall";
+        } elseif($season == 1) {
+            $seasonName = "winter";
+        } elseif($season == 2) {
+            $seasonName = "spring";
+        } elseif($season == 3) {
+            $seasonName = "summer";
+        }
+        return $seasonName;
+    }
+
+    public static function sendRequest($endpoint, $data = [], $method = self::METHOD_GET)
+    {
+        if(config('gameserver.base_url') == null) {
+            return ['errors' => []];
+        }
+        if (is_string($data)) {
+            $method = $data;
+            $data = [];
+        }
+
+        $url = config('gameserver.base_url') . $endpoint;
+        $response = Http::withHeaders([
+        ])->$method($url, $data);
+        $decodedResponse = json_decode($response, true);
+        return $decodedResponse;
+    }
+
+}

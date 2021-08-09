274da5496b580ca1020c4897a3a42f6f6a6cc84f
Daniel S. Billing <daniel.billing@intility.no>
2021-08-09 21:10:24 +0200

    Get the correct gj account id

--- resources/views/skin-subdomain/home.blade.php
+++ resources/views/skin-subdomain/home.blade.php
@@ -9,17 +9,17 @@
                 <div class="bg-white dark:bg-gray-800 w-full p-4">
                     <div class="text-gray-800 dark:text-white text-xl font-medium mb-2">Your Current Skin </div>
                     <div class="card-body">
-                        @if(File::exists(public_path('player/'.$id.'.png')))
-                            <img src="{{ asset('player/'.$id.'.png') }}?r={{ now()->timestamp }}" class="mx-auto my-4">
+                        @if(File::exists(public_path('player/'.Auth::user()->gamejolt->id.'.png')))
+                            <img src="{{ asset('player/'.Auth::user()->gamejolt->id.'.png') }}?r={{ now()->timestamp }}" class="mx-auto my-4">
                         @else
                             <p>No skins have been added to your account yet.</p>
-                            @if(\App\Models\GJUser::find(session()->get('gjid'))->skins()->count() >= env('SKIN_MAX_UPLOAD'))
+                            @if(Auth::user()->gamejolt->skins()->count() >= env('SKIN_MAX_UPLOAD'))
                                 <div class="px-4 py-3 mt-4 leading-normal text-blue-800 bg-blue-300 rounded-lg text-sm">
                                     <i class="fas fa-info-circle mr-2" aria-hidden="true"></i> Your slots are full. You cannot import from the old site unless you delete one of the slots.
                                 </div>
                             @else
                                 <p class="text-sm mt-3">
-                                    <a href="{{ route('import', $id) }}" class="w-full inline-block items-center py-2 px-4 mt-2 text-blue-900 transition-colors duration-150 bg-blue-200 rounded-lg focus:shadow-outline hover:bg-blue-300">
+                                    <a href="{{ route('import', Auth::user()->gamejolt->id) }}" class="w-full inline-block items-center py-2 px-4 mt-2 text-blue-900 transition-colors duration-150 bg-blue-200 rounded-lg focus:shadow-outline hover:bg-blue-300">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                                         </svg>
@@ -29,7 +29,7 @@
                             @endif
                         @endif
                     </div>
-                    @if(File::exists(public_path('player/'.$id.'.png')))
+                    @if(File::exists(public_path('player/'.Auth::user()->gamejolt->id.'.png')))
                         <div class="mt-2">
                             <a class="inline-flex items-center h-8 px-4 text-sm text-blue-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800" href="{{ route('player-skin-duplicate') }}">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -37,7 +37,7 @@
                                 </svg>
                                 Save to "My skins"
                             </a>
-                            @if(File::exists(public_path('player/'.$id.'.png')))
+                            @if(File::exists(public_path('player/'.Auth::user()->gamejolt->id.'.png')))
                                 <a class="inline-flex items-center h-8 px-4 ml-1 text-sm text-red-100 transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline hover:bg-red-800" href="{{ route('player-skin-destroy') }}">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />

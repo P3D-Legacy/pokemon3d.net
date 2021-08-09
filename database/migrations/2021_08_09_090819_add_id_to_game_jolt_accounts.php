da5b521f0c02c2c36db74f0074eccf462c582690
Daniel S. Billing <daniel.billing@intility.no>
2021-08-09 11:31:49 +0200

    Added gamejolt account id to game_jolt_accounts

---
+++ database/migrations/2021_08_09_090819_add_id_to_game_jolt_accounts.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIdToGameJoltAccounts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('game_jolt_accounts', function (Blueprint $table) {
+            $table->dropColumn('id');
+            $table->bigInteger('aid')->unsigned()->primary()->first()->comment('GameJolt Account ID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('game_jolt_accounts', function (Blueprint $table) {
+            $table->dropColumn('aid');
+        });
+    }
+}

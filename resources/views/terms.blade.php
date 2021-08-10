473a34fe7a2a596ae084c47a709193fd8d262243
Daniel S. Billing <daniel.billing@intility.no>
2021-08-10 08:10:51 +0200

    Updated terms and policy styling

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -7,7 +7,7 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
+            <div class="w-full sm:max-w-2xl mt-6 mb-10 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
                 {!! $terms !!}
             </div>
         </div>

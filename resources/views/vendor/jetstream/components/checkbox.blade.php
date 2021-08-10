4a2f6c10f67a35989e9bac8c1609663cab598125
Daniel S. Billing <daniel.billing@intility.no>
2021-08-10 11:46:29 +0200

    Updated some styling for checkbox

--- resources/views/vendor/jetstream/components/checkbox.blade.php
+++ resources/views/vendor/jetstream/components/checkbox.blade.php
@@ -1,1 +1,1 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-green-700 shadow-sm focus:border-green-500 focus:ring focus:ring-green-300 focus:ring-opacity-50']) !!}>

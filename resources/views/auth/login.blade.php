d7b6a25d01ab0064060c5a7a831aa075c3b0d4b5
Daniel S. Billing <daniel.billing@intility.no>
2021-08-13 08:48:45 +0200

    Update login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,8 +18,8 @@
             @csrf
 
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-label for="email" value="{{ __('Email or Username') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">

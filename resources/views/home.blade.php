02d74d896f24140f41a84a4a1a37c42bface7a55
Daniel S. Billing <daniel.billing@intility.no>
2021-08-16 21:40:03 +0200

    Added blog routes to home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,6 +20,13 @@
             <!-- Responsive Navigation Menu -->
             <div :class="{'flex': open, 'hidden': ! open}" class="hidden w-full pt-2">
                 <ul class="list-reset lg:flex flex-1 place-items-end space-y-1 grid justify-items-end">
+                    <x-home.responsive-nav-link title="Blog" url="{{ route('blog.index') }}">
+                        <x-slot name="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                              </svg>
+                        </x-slot>
+                    </x-home.responsive-nav-link>
                     <x-home.responsive-nav-link title="Forum" url="https://pokemon3d.net/forum/">
                         <x-slot name="icon">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -69,7 +76,14 @@
             <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 p-4 lg:p-0 z-20"
                 id="nav-content">
                 <ul class="list-reset lg:flex justify-end flex-1 items-center">
-                   <x-home.nav-link title="Forum" url="https://pokemon3d.net/forum/">
+                    <x-home.nav-link title="Blog" url="{{ route('blog.index') }}">
+                        <x-slot name="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                              </svg>
+                        </x-slot>
+                    </x-home.nav-link>
+                    <x-home.nav-link title="Forum" url="https://pokemon3d.net/forum/">
                         <x-slot name="icon">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
@@ -364,7 +378,7 @@
                         {{ config('app.name') }}
                     </p>
                     <ul class="list-reset mb-6">
-                        <x-home.footer-link title="Official Blog" url="#" />
+                        <x-home.footer-link title="Official Blog" url="{{ route('blog.index') }}" />
                         <x-home.footer-link title="About Us" url="#" />
                         <x-home.footer-link title="Contact" url="#" />
                     </ul>

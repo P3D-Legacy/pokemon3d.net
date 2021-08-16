b2631c6cf5854ca2ca46ac9e54edcb52a7f52847
Daniel S. Billing <daniel.billing@intility.no>
2021-08-16 21:52:18 +0200

    Removed unused import

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use GrahamCampbell\Markdown\Facades\Markdown;
 
 class PostController extends Controller
 {

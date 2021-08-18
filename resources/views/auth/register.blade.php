9d4cd4cd3672d90c77e232a9f29fc0740b146f2f
Daniel S. Billing <daniel.billing@intility.no>
2021-08-18 20:03:52 +0200

    Testing register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
 
         <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('register') }}">
+        <form method="POST" action="{{ route('register-store') }}">
             @csrf
 
             <div>

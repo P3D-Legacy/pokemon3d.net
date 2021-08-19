184ae23de6249b55442689c06388509b42291cf3
Daniel S. Billing <daniel.billing@intility.no>
2021-08-19 21:22:53 +0200

    Updated view for posts admin

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,24 +1,91 @@
 <x-app-layout>
-<main class="text-gray-800 px-8 md:w-2/3 lg:w-3/5 xl:w-1/2 sm:w-full mx-auto">
+	<x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Blog Posts') }}
+        </h2>
+    </x-slot>
+	
+	<div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+				<div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+					<table class="min-w-full leading-normal">
+						<thead>
+							<tr>
+								<th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal">
+									Title
+								</th>
+								<th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+									Created
+								</th>
+								<th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+									Updated
+								</th>
+								<th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+									Status
+								</th>
+								<th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach ($posts as $post)
+								<tr>
+									<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+										<p class="text-gray-900 whitespace-no-wrap">
+											{{ Str::limit($post->title, 100) }}
+										</p>
+									</td>
+									<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+										<p class="text-gray-900 whitespace-no-wrap">
+											{{ $post->created_at->diffForHumans() }}
+										</p>
+									</td>
+									<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+										<p class="text-gray-900 whitespace-no-wrap">
+											{{ $post->updated_at->diffForHumans() }}
+										</p>
+									</td>
+									<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+										@if($post->active)
+											<span class="inline-flex items-center justify-center px-2 py-1 text-xs leading-none text-green-900 bg-green-200 rounded-full capitalize">
+												Active
+											</span>
+										@else
+											<span class="inline-flex items-center justify-center px-2 py-1 text-xs leading-none text-gray-900 bg-gray-200 rounded-full capitalize">
+												Hidden
+											</span>
+										@endif
+									</td>
+									<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+										<a href="{{ route('blog.show', $post->uuid) }}" class="py-1 px-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg mr-1">
+											<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+											</svg>
+											Show
+										</a>
+										<a href="#" class="py-1 px-2 bg-yellow-600 hover:bg-yellow-700 focus:ring-yellow-500 focus:ring-offset-yellow-200 text-white w-full transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg mr-1">
+											<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+											  </svg>
+											Edit
+										</a>
+										<a href="#" class="py-1 px-2 bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg mr-1">
+											<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+											  </svg>
+											Delete
+										</a>
+									</td>
+								</tr>
+							@endforeach
+						</tbody>
+					</table>
 
-    <div class="text-center">
-        <h1 class="text-center text-3xl my-8">Posts</h1>
-        <a class="my-3 hover:underline" href="{{route('posts.create')}}">New post</a>
-    </div>
-
-    @foreach ($posts as $post)
-      <section class="border-b">
-        <h2 class="text-3xl font-bold text-center mt-4 hover:underline"><a href="{{Route('posts.show', $post->slug)}}">{{$post->title}}</a></h2>
-        <p class="text-sm text-center leading-5 text-gray-700 mt-3">Posted {{$post->updated_at->format('d/m/Y')}}</p>
-        <article class="markdown-body">
-        {!! $post->summary_html !!}
-          <div class="text-right mt-8">
-            <a href="{{Route('blog.show', $post->slug)}}" role="button" class="px-4 py-2 border border-gray-300 rounded bg-white text-sm font-medium text-gray-700 hover:border-gray-500 focus:z-10 focus:outline-none focus:border-gray-300 focus:shadow-outline-gray active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-              Read more
-            </a>
-          </div>
-        </article>
-      </section>
-    @endforeach
-</main>
+				</div>
+				{!! $posts->links() !!}
+			</div>
+		</div>
+	</div>
 </x-app-layout>

4e2475668673fd31e94680b613fd680c4e81c941
Daniel S. Billing <daniel.billing@intility.no>
2021-08-19 16:10:55 +0200

    Updated QA message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                                     </svg>
-                                    APP NOT IN PRODUCTION MODE!
+                                    {{ (env('APP_ENV') == 'staging') ? 'QA: FOR TESTING ONLY' : 'DEV MODE' }}
                                 </p>
                             </div>
                         </div>

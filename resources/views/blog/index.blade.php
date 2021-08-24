4899fbf91ff79e56791411cd834e25b1f377492e
Daniel S. Billing <daniel.billing@intility.no>
2021-08-24 19:25:47 +0200

    Added nearestK to counts for posts

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -61,12 +61,12 @@
 											<p class="text-gray-400 dark:text-gray-300">
 												<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 													<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-												</svg> {{ $post->likers()->count() }}
+												</svg> {{ App\Helpers\NumberHelper::nearestK($post->likers()->count()) }}
 												&nbsp;
 												<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 													<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
 													<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-												</svg> {{ views($post)->count() }}
+												</svg> {{ App\Helpers\NumberHelper::nearestK(views($post)->count()) }}
 											</p>
 										</div>
 									</div>

49016733a7364771296f576a7eb5dbfd8131edbd
Daniel S. Billing <daniel.billing@intility.no>
2021-08-24 18:40:03 +0200

    Updated like count for index

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -61,7 +61,7 @@
 											<p class="text-gray-400 dark:text-gray-300">
 												<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 													<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-												</svg> 1234
+												</svg> {{ $post->likers()->count() }}
 												&nbsp;
 												<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 													<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />

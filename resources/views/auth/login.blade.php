cb151031b877c08ed5fec3093239a6c1838106e2
Daniel S. Billing <daniel.billing@intility.no>
2021-09-01 18:33:44 +0200

    Let's hide the message when we are on the new year

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,18 +6,20 @@
             </a>
         </x-slot>
 
-        <div class="px-4 py-3 leading-normal text-blue-700 bg-blue-100 rounded-lg text-sm" role="alert">
-            <p>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-                This new website has now a own authentication. Please <a class="font-bold hover:underline" href="{{ env('LOGIN_AUTH_BLOG_POST') ?? route('blog.index') }}">read the blog post</a> for more information!
-            </p>
-        </div>
+        @if(\Carbon\Carbon::createFromFormat('Y-m-d', '2021-12-31')->isFuture())
+            <div class="px-4 py-3 leading-normal text-blue-700 bg-blue-100 rounded-lg text-sm" role="alert">
+                <p>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    This new website has now a own authentication. Please <a class="font-bold hover:underline" href="{{ env('LOGIN_AUTH_BLOG_POST') ?? route('blog.index') }}">read the blog post</a> for more information!
+                </p>
+            </div>
 
-        <div class="w-1/2 mx-auto">
-            <x-jet-section-border />
-        </div>
+            <div class="w-1/2 mx-auto">
+                <x-jet-section-border />
+            </div>
+        @endif
 
         <x-jet-validation-errors class="mb-4" />
 

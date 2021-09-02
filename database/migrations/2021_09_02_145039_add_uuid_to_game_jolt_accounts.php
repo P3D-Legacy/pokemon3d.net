b0063868c23fe6495db3566c2520c0f5f0439f2a
Daniel S. Billing <daniel.billing@intility.no>
2021-09-02 20:13:18 +0200

    Added UUID to GameJoltAccount

---
+++ database/migrations/2021_09_02_145039_add_uuid_to_game_jolt_accounts.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUuidToGameJoltAccounts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('game_jolt_accounts', function (Blueprint $table) {
+            $table->dropPrimary();
+            $table->uuid('uuid')->primary()->first();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('game_jolt_accounts', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        });
+    }
+}

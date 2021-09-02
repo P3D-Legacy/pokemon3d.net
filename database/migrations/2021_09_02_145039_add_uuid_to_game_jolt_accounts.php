2ab8c95fe0f5dd11c432a1690299404d3a4fba9d
Daniel S. Billing <daniel.billing@intility.no>
2021-09-02 20:35:47 +0200

    Revert "Update 2021_09_02_145039_add_uuid_to_game_jolt_accounts.php"
    This reverts commit 6f6cd4a4432b9b9f1582898cd28b7d6cb52b8a4f.

--- database/migrations/2021_09_02_145039_add_uuid_to_game_jolt_accounts.php
+++ database/migrations/2021_09_02_145039_add_uuid_to_game_jolt_accounts.php
@@ -1,9 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddUuidToGameJoltAccounts extends Migration
 {
@@ -16,7 +15,7 @@ class AddUuidToGameJoltAccounts extends Migration
     {
         Schema::table('game_jolt_accounts', function (Blueprint $table) {
             $table->dropPrimary('id');
-            $table->uuid('uuid')->primary()->default(DB::raw('(UUID())'))->first();
+            $table->uuid('uuid')->primary()->first();
         });
     }
 

09dcc685f474c5c72311d86521df3ede9a57dc07
Daniel S. Billing <daniel.billing@intility.no>
2021-09-02 21:51:29 +0200

    Added admin menu to mobile

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -193,6 +193,42 @@
             </x-jet-responsive-nav-link>
         </div>
 
+        <!-- Mobile admin menu -->
+        @role('super-admin|admin')
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                <div class="flex items-center px-4">
+                    <div class="text-base font-medium text-gray-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                        </svg>
+                        {{ __('Admin') }}
+                    </div>
+                </div>
+                <div class="mt-3 space-y-1">
+                    <div class="block px-4 py-2 text-xs text-gray-400">
+                        {{ __('Content') }}
+                    </div>
+                    <x-jet-responsive-nav-link href="{{ route('posts.index') }}">
+                        {{ __('Blog Posts') }}
+                    </x-jet-responsive-nav-link>
+                    <div class="block px-4 py-2 text-xs text-gray-400">
+                        {{ __('User Management') }}
+                    </div>
+                    <x-jet-responsive-nav-link href="{{ route('users.index') }}">
+                        {{ __('Users') }}
+                    </x-jet-responsive-nav-link>
+                    <x-jet-responsive-nav-link href="{{ route('roles.index') }}">
+                        {{ __('Roles') }}
+                    </x-jet-responsive-nav-link>
+                    @role('super-admin') 
+                        <x-jet-responsive-nav-link href="{{ route('permissions.index') }}">
+                            {{ __('Permissions') }}
+                        </x-jet-responsive-nav-link>
+                    @endrole
+                </div>
+            </div>
+        @endrole
+
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="flex items-center px-4">

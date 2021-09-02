a9dea70a63903b165bfbd0723b315a4f5e75bc08
Daniel S. Billing <daniel.billing@intility.no>
2021-09-02 21:18:10 +0200

    Update 2021_09_02_145140_add_uuid_to_forum_accounts.php

--- database/migrations/2021_09_02_145140_add_uuid_to_forum_accounts.php
+++ database/migrations/2021_09_02_145140_add_uuid_to_forum_accounts.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class AddUuidToForumAccounts extends Migration
 {
@@ -27,6 +27,7 @@ class AddUuidToForumAccounts extends Migration
     public function down()
     {
         Schema::table('forum_accounts', function (Blueprint $table) {
+            $table->id();
             $table->dropColumn('uuid');
         });
     }

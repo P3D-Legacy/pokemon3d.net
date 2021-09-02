9d48e287aaa96a691f0d6670764333084ef08d45
Daniel S. Billing <daniel.billing@intility.no>
2021-09-02 08:49:24 +0200

    Added icon to forum login modal

--- resources/views/livewire/login/xenforo.blade.php
+++ resources/views/livewire/login/xenforo.blade.php
@@ -1,6 +1,8 @@
 <x-jet-form-section submit="save">
     <x-slot name="title">
-        Forum Account
+        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-5 h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+        </svg>Forum Account
     </x-slot>
 
     <x-slot name="description">
@@ -10,13 +12,13 @@
     <x-slot name="form">
         <div class="col-span-6">
             <x-jet-label for="username" value="{{ __('Email or Username') }}" />
-            <x-jet-input id="username" type="text" class="mt-1 block w-full" wire:model="username" />
+            <x-jet-input id="username" type="text" class="block w-full mt-1" wire:model="username" />
             <x-jet-input-error for="username" class="mt-2" />
         </div>
 
         <div class="col-span-6">
             <x-jet-label for="password" value="{{ __('Password') }}" />
-            <x-jet-input id="password" type="password" class="mt-1 block w-full" wire:model="password" />
+            <x-jet-input id="password" type="password" class="block w-full mt-1" wire:model="password" />
             <x-jet-input-error for="password" class="mt-2" />
         </div>
     </x-slot>

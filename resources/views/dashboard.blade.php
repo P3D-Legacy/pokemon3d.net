585fb81a911b1adfa766b5151c77025417d3a95f
Daniel S. Billing <daniel.billing@intility.no>
2021-09-16 21:29:45 +0200

    Updated header with dark mode

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>

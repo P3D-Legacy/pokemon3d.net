8a10dc6227f0c495f4a52230eb7842cea4f3a66c
Daniel S. Billing <daniel.billing@intility.no>
2021-09-16 21:28:58 +0200

    Dark mode for navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,7 @@
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
+                <header class="bg-white shadow dark:bg-black">
                     <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>

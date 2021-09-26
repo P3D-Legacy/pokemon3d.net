a247f6058f83dc0b7513ffa50c1d5b9727ffddb6
Daniel S. Billing <daniel.billing@intility.no>
2021-09-26 22:18:09 +0200

    Create PingAllServers.php

---
+++ app/Console/Commands/PingAllServers.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Server;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class PingAllServers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'server:pingall';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Ping all servers to update their satus.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $servers = Server::all();
+        foreach ($servers as $server) {
+            Artisan::call('ping:server '.$server->uuid);
+        }
+        $this->info('All servers have been pinged.');
+        return;
+    }
+}

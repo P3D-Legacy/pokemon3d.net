94cd6e4885a88e6708c2e3e815a945c39c11f6f4
Daniel S. Billing <daniel.billing@intility.no>
2021-09-27 07:49:13 +0200

    Server Controller, views and routes

---
+++ app/Http/Controllers/ServerController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Server;
+use Illuminate\Http\Request;
+
+class ServerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view("server.index");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('server.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => [
+                'string',
+                'required',
+            ],
+            'host' => [
+                'regex:[^\:]+:[0-9]{5}',
+                'required',
+            ],
+            'description' => [
+                'string',
+                'required',
+            ],
+        ]);
+        Server::create($validatedData);
+        session()->flash('flash.banner', 'Created Server!');
+        session()->flash('flash.bannerStyle', 'success');
+        return redirect()->route('server.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return redirect()->route('server.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $server = Server::findOrFail($id);
+        if($server) {
+            $server->delete();
+            session()->flash('flash.banner', 'Deleted Server!');
+            session()->flash('flash.bannerStyle', 'success');
+        }
+        return redirect()->route('server.index');
+    }
+}

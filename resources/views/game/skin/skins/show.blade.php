f46c3f9c70d13faf3aa5cdd7fef506a14bb90ada
Daniel S. Billing <daniel.billing@intility.no>
2021-10-04 21:27:51 +0200

    Update show.blade.php

--- resources/views/game/skin/skins/show.blade.php
+++ resources/views/game/skin/skins/show.blade.php
@@ -5,8 +5,11 @@
         </h2>
     </x-slot>
 
-    <div class="mt-4">
-        @include('game.skin.component.card', ['skin' => $skin])
+    
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            @include('game.skin.component.card', ['skin' => $skin])
+        </div>
     </div>
 
 </x-app-layout>

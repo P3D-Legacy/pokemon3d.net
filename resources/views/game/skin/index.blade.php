ba45c6ffacd52b196da12edb5cbd6e4b6bbc81a1
Daniel S. Billing <daniel.billing@intility.no>
2021-10-07 20:44:39 +0200

    No need for lg rounded on btn

--- resources/views/game/skin/index.blade.php
+++ resources/views/game/skin/index.blade.php
@@ -24,7 +24,7 @@
                                             </div>
                                         @else
                                             <p class="mt-3 text-sm">
-                                                <a href="{{ route('import', Auth::user()->gamejolt->id) }}" class="items-center inline-block w-full px-4 py-2 mt-2 text-blue-900 transition-colors duration-150 bg-blue-200 rounded-lg focus:shadow-outline hover:bg-blue-300">
+                                                <a href="{{ route('import', Auth::user()->gamejolt->id) }}" class="items-center inline-block w-full px-4 py-2 mt-2 text-blue-900 transition-colors duration-150 bg-blue-200 rounded focus:shadow-outline hover:bg-blue-300">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                                                     </svg>
@@ -36,14 +36,14 @@
                                 </div>
                                 @if(File::exists(public_path('player/'.Auth::user()->gamejolt->id.'.png')))
                                     <div class="mt-2">
-                                        <a class="inline-flex items-center h-8 px-4 text-sm text-blue-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800" href="{{ route('player-skin-duplicate') }}">
+                                        <a class="inline-flex items-center h-8 px-4 text-sm text-blue-100 transition-colors duration-150 bg-blue-700 rounded focus:shadow-outline hover:bg-blue-800" href="{{ route('player-skin-duplicate') }}">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16v2a2 2 0 01-2 2H5a2 2 0 01-2-2v-7a2 2 0 012-2h2m3-4H9a2 2 0 00-2 2v7a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-1m-1 4l-3 3m0 0l-3-3m3 3V3" />
                                             </svg>
                                             Save to "My skins"
                                         </a>
                                         @if(File::exists(public_path('player/'.Auth::user()->gamejolt->id.'.png')))
-                                            <a class="inline-flex items-center h-8 px-4 ml-1 text-sm text-red-100 transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline hover:bg-red-800" href="{{ route('player-skin-destroy') }}">
+                                            <a class="inline-flex items-center h-8 px-4 ml-1 text-sm text-red-100 transition-colors duration-150 bg-red-700 rounded focus:shadow-outline hover:bg-red-800" href="{{ route('player-skin-destroy') }}">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                                 </svg>

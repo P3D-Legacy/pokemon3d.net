ceba60b9f5f3ea1b95b2c8eb550a627faf91e1ec
Daniel S. Billing <daniel.billing@intility.no>
2021-10-12 19:16:17 +0200

    Installed andreaselia/laravel-api-to-postman

---
+++ config/api-postman.php
@@ -0,0 +1,118 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base URL
+    |--------------------------------------------------------------------------
+    |
+    | The base URL for all of your endpoints.
+    |
+    */
+
+    'base_url' => env('APP_URL', 'http://localhost'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Collection Filename
+    |--------------------------------------------------------------------------
+    |
+    | The name for the collection file to be saved.
+    |
+    */
+
+    'filename' => '{timestamp}_{app}_collection.json',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Structured
+    |--------------------------------------------------------------------------
+    |
+    | If you want folders to be generated based on namespace.
+    |
+    */
+
+    'structured' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Auth Middleware
+    |--------------------------------------------------------------------------
+    |
+    | The middleware which wraps your authenticated API routes.
+    |
+    | E.g. auth:api, auth:sanctum
+    |
+    */
+
+    'auth_middleware' => 'auth:sanctum',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Headers
+    |--------------------------------------------------------------------------
+    |
+    | The headers applied to all routes within the collection.
+    |
+    */
+
+    'headers' => [
+        [
+            'key' => 'Accept',
+            'value' => 'application/json',
+        ],
+        [
+            'key' => 'Content-Type',
+            'value' => 'application/json',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Form Data
+    |--------------------------------------------------------------------------
+    |
+    | Determines whether or not form data should be handled.
+    |
+    */
+
+    'enable_formdata' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Form Data
+    |--------------------------------------------------------------------------
+    |
+    | The key/values to requests for form data dummy information.
+    |
+    */
+
+    'formdata' => [
+        // 'email' => 'john@example.com',
+        // 'password' => 'changeme',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Include Middleware
+    |--------------------------------------------------------------------------
+    |
+    | The routes of the included middleware are included in the export.
+    |
+    */
+
+    'include_middleware' => ['api'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Disk Driver
+    |--------------------------------------------------------------------------
+    |
+    | Specify the configured disk for storing the postman collection file.
+    |
+    */
+
+    'disk' => 'local',
+
+];

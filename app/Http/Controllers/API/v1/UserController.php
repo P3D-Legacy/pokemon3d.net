03a40fddf9548ad1f37412536f2b50c3f71f73b3
Daniel S. Billing <daniel.billing@intility.no>
2021-10-13 17:03:09 +0200

    Commented out permissions in api controllers

--- app/Http/Controllers/API/v1/UserController.php
+++ app/Http/Controllers/API/v1/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['permission:api']);
+        //$this->middleware(['permission:api']);
     }
 
     /**

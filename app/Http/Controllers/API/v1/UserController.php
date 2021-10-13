25301946072767c57c09053eb498036a5e6482b1
Daniel S. Billing <daniel.billing@intility.no>
2021-10-13 17:05:45 +0200

    Moved UserResource

--- app/Http/Controllers/API/v1/UserController.php
+++ app/Http/Controllers/API/v1/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API\v1;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\v1\UserResource;
+use App\Http\Resources\API\v1\UserResource;
 
 class UserController extends Controller
 {

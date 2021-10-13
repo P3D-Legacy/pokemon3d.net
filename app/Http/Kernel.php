c7860c5acc9c94d9b62b0e18ab1985a7fa7ffd95
Daniel S. Billing <daniel.billing@intility.no>
2021-10-13 17:03:34 +0200

    Added API JSON Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -70,5 +70,6 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        'api.json' => \App\Http\Middleware\ApiJsonMiddleware::class,
     ];
 }
---
+++ app/Http/Middleware/ApiJsonMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ApiJsonMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->add(['accept' => 'application/json']);
+        return $next($request);
+    }
+}

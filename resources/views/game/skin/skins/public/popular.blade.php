42b89e5e6730d08967ff201ee5cd4f51d49c3c26
Daniel S. Billing <daniel.billing@intility.no>
2021-10-15 08:07:53 +0200

    Added better styling to tab buttons for public skins

--- resources/views/game/skin/skins/public/popular.blade.php
+++ resources/views/game/skin/skins/public/popular.blade.php
@@ -8,8 +8,8 @@
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="flex items-center mb-4">
-                <a class="border-l border-t border-b text-base font-medium rounded-l-md hover:bg-gray-100 px-4 py-2 {{ request()->is('game/skin/public/new*') ? 'text-green-800 bg-green-50' : 'text-gray-800' }} bg-white" href="{{ route('skins-newest') }}">Newest</a>
-                <a class="border-t border-b border-r text-base font-medium rounded-r-md hover:bg-gray-100 px-4 py-2 {{ request()->is('game/skin/public/popular*') ? 'text-green-800 bg-green-50' : 'text-gray-800 bg-white' }}" href="{{ route('skins-popular') }}">Most Popular</a>
+                <a class="border-l border-t border-b text-base font-medium rounded-l-md hover:bg-green-50 px-4 py-2 {{ request()->is('game/skin/public/new*') ? 'text-green-800 bg-green-100 dark:text-green-300 dark:bg-green-800 dark:border-green-700 dark:hover:bg-green-700' : 'text-gray-800 bg-white dark:bg-gray-700 dark:text-gray-200 dark:border-gray-600 dark:hover:bg-gray-600' }}" href="{{ route('skins-newest') }}">Newest</a>
+                <a class="border-t border-b border-r text-base font-medium rounded-r-md hover:bg-green-50 px-4 py-2 {{ request()->is('game/skin/public/popular*') ? 'text-green-800 bg-green-100 dark:text-green-300 dark:bg-green-800 dark:border-green-700 dark:hover:bg-green-700' : 'text-gray-800 bg-white dark:bg-gray-700 dark:text-gray-200 dark:border-gray-600 dark:hover:bg-gray-600' }}" href="{{ route('skins-popular') }}">Most Popular</a>
             </div>
 
             <div class="grid grid-flow-row grid-cols-1 gap-4 auto-rows-max sm:grid-cols-2 lg:grid-cols-3">

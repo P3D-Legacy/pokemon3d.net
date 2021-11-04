1db5852493fb78cf7e8a365ec77b7a584e53cdac
Daniel S. Billing <daniel.billing@intility.no>
2021-11-04 19:09:12 +0100

    Added create gamejolt account ban endpoint

--- app/Http/Controllers/API/v1/GamejoltAccountBanController.php
+++ app/Http/Controllers/API/v1/GamejoltAccountBanController.php
@@ -36,6 +36,45 @@ class GamejoltAccountBanController extends Controller
     }
 
     /**
+     * Store a newly created resource in storage.
+     *
+     * @bodyParam gamejolt_account_id int required The ID of the Gamejolt Account.
+     * @bodyParam reason_id int required The ID of the Ban Reason.
+     * @bodyParam expires_at date optional The expiry of the ban.
+     * 
+     * @response 201 {
+     *       "data": {
+     *           "gamejoltaccount_id": 12345,
+     *           "reason_id": 1,
+     *           "expires_at": "2021-02-01T00:00:00.000000Z",
+     *           "banned_by_id": 1,
+     *           "uuid": "1830ef92-b58b-4671-9096-2b7741c0b0d8",
+     *           "updated_at": "2021-01-01T17:57:10.000000Z",
+     *           "created_at": "2021-01-01T17:57:10.000000Z"
+     *       }
+     *   }
+     */
+    public function store(Request $request)
+    {
+        if (!$request->user()->tokenCan('create')) {
+            return response()->json([
+                'error' => 'Token does not have access!',
+            ]);
+        }
+        $request->validate([
+            'gamejoltaccount_id' => 'required|integer',
+            'reason_id' => 'required|integer',
+            'expire_at' => 'nullable|date',
+        ]);
+        $data = $request->all();
+        $data = array_merge($data, [
+            'banned_by_id' => $request->user()->id,
+        ]);
+        $resource = GamejoltAccountBan::create($data);
+        return new GamejoltAccountBanResource($resource);
+    }
+
+    /**
      * Display the specified resource.
      *
      * @urlParam id int required The ID of the Gamejolt Account.

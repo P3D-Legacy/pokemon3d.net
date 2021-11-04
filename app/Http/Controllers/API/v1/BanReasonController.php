6a55d3b7c0c6c798ac448b08acf5d471ecaca178
Daniel S. Billing <daniel.billing@intility.no>
2021-11-04 19:42:30 +0100

    Added Ban Reason show endpoint

--- app/Http/Controllers/API/v1/BanReasonController.php
+++ app/Http/Controllers/API/v1/BanReasonController.php
@@ -34,4 +34,34 @@ class BanReasonController extends Controller
         $resources = BanReason::all();
         return new BanReasonResource($resources);
     }
+
+    /**
+     * Display the specified resource.
+     *
+     * @urlParam id int required The UUID of the ban reason.
+     * 
+     * @response {
+     *    "data": [
+     *        {
+     *            "id": 1,
+     *            "uuid": "1830ef92-b58b-4671-9096-2b7741c0b0d8",
+     *            "name": "Abusing in-game glitches",
+     *            "user_id": 1,
+     *            "created_at": "2021-01-01T17:57:10.000000Z",
+     *            "updated_at": "2021-01-01T17:57:10.000000Z",
+     *            "deleted_at": null
+     *        },
+     *    ]
+     * }
+     */
+    public function show(Request $request, $id)
+    {
+        if (!$request->user()->tokenCan('read')) {
+            return response()->json([
+                'error' => 'Token does not have access!',
+            ]);
+        }
+        $resource = BanReason::findOrFail($id);
+        return new BanReasonResource($resource);
+    }
 }

4e375f45fef7244bf6c19663c5338091898da4c0
Daniel S. Billing <daniel.billing@intility.no>
2021-11-04 18:28:49 +0100

    Update UserController.php

--- app/Http/Controllers/API/v1/UserController.php
+++ app/Http/Controllers/API/v1/UserController.php
@@ -22,12 +22,12 @@ class UserController extends Controller
      */
     public function show(Request $request, $id)
     {
-        $user = User::with(['roles.permissions', 'gamejolt', 'forum'])->findOrFail($id);
         if (!$request->user()->tokenCan('read')) {
             return response()->json([
                 'error' => 'Token does not have access!',
             ]);
         }
+        $user = User::with(['roles.permissions', 'gamejolt', 'forum'])->findOrFail($id);
         return new UserResource($user);
     }
 

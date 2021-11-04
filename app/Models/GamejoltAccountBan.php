545d189ef46973ff68e506f12a3dd0de10e41457
Daniel S. Billing <daniel.billing@intility.no>
2021-11-04 22:14:27 +0100

    Fixed typo

--- app/Models/GamejoltAccountBan.php
+++ app/Models/GamejoltAccountBan.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Models\GamejoltAccount;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -63,7 +62,7 @@ class GamejoltAccountBan extends Model
      */
     public function gamejoltaccount()
     {
-        return $this->hasOne(GamejoltAccount::class, 'id', 'gamejoltaccount_id');
+        return $this->hasOne(GameJoltAccount::class, 'id', 'gamejoltaccount_id');
     }
 
     /**

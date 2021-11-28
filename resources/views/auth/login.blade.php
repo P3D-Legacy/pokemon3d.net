4a964abc3a965919e45be6bfafb16b0ca6ac3419
Daniel S. Billing <daniel.billing@intility.no>
2021-11-28 19:51:54 +0100

    Added icon to error message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,9 @@
 
 		@if(Session::has('error'))
 			<div class="relative px-4 py-3 mb-2 leading-normal text-red-700 bg-red-100 rounded-lg" role="alert">
+				<svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+				</svg>
 				{{ Session::get('error') }}
 			</div>
 		@endif

e5300ee6d1f9fabb87df8d2a1ae3684eef2d8cc0
Daniel S. Billing <daniel.billing@intility.no>
2021-11-28 16:34:55 +0100

    Added model and migration for Discord Account

---
+++ database/migrations/2021_11_28_144353_create_discord_accounts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscordAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discord_accounts', function (Blueprint $table) {
+            $table->increments('aid');
+            $table->uuid('uuid')->unique();
+            $table->integer('did')->comment('Discord ID');
+            $table->text('username')->comment('Discord Username');
+            $table->text('email')->comment('Discord Email');
+            $table->text('avatar')->comment('Discord Avatar URL');
+            $table->integer('discriminator')->comment('Discord Discriminator');
+            $table->timestamp('verified_at')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discord_accounts');
+    }
+}

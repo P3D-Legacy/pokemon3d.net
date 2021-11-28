a66522373c7e65c86dbd77c92a847f73c5868978
Daniel S. Billing <daniel.billing@intility.no>
2021-11-28 16:36:38 +0100

    Reworked auth for discord

--- app/Http/Controllers/Auth/DiscordController.php
+++ app/Http/Controllers/Auth/DiscordController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Actions\Fortify\CreateNewUser;
+use App\Models\DiscordAccount;
 use Laravel\Socialite\Facades\Socialite;
 use GuzzleHttp\Exception\ClientException;
 use Laravel\Socialite\Two\InvalidStateException;
@@ -31,23 +32,29 @@ class DiscordController extends Controller
     {
         try {
             $discordUser = Socialite::driver('discord')->user();
+            
+            if (!$discordUser->user['verified']) {
+                return redirect()->route('login')->withError('Discord user not verified.');
+            }
+
             $userProfile = [
+                'id' => $discordUser->id,
                 'username' => $discordUser->name,
                 'email' => $discordUser->email,
+                'avatar' => $discordUser->avatar,
+                'discriminator' => $discordUser->user['discriminator'],
             ];
 
             // Check if user exists with email
-            $user = User::where('email', $discordUser->email)->first();
+            $discordAccount = DiscordAccount::where('did', $userProfile['id'])->first();
+            $user = $discordAccount ? $discordAccount->user : null;
             if ($user) {
                 Auth::login($user);
                 return redirect()->route('dashboard');
+            } else {
+                // User not found
+                return redirect()->route('login')->withError('Discord account association not found.');
             }
-
-            // Create a new user if not found
-            $creator = new CreateNewUser();
-            $createdUser = $creator->create($userProfile);
-            // Login the new user
-            Auth::login($createdUser);
         } catch (InvalidStateException $e) {
             return redirect()->route('home')->withError('Something went wrong with Discord login. Please try again.');
         } catch (ClientException $e) {

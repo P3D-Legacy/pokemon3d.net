f302b657ac09ade351e1cdc451a9f867bf1f13b8
Daniel S. Billing <daniel.billing@intility.no>
2021-12-07 19:35:26 +0100

    Headwind + sticky

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
 	<x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight dark:text-gray-200">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             {{ __('Blog Posts') }} 
-			<a href="{{ route('posts.create') }}" class="ml-4 py-1 px-2 bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+			<a href="{{ route('posts.create') }}" class="w-full px-2 py-1 ml-4 text-base font-semibold text-center text-white transition duration-200 ease-in bg-green-600 rounded-lg shadow hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2">
+				<svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
 				</svg>
 				Create
@@ -12,73 +12,80 @@
     </x-slot>
 	
 	<div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-			<div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-				<div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+			<div class="px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8">
+				<div class="inline-block min-w-full overflow-hidden rounded-lg shadow">
 					<table class="min-w-full leading-normal">
 						<thead>
 							<tr>
-								<th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal">
+								<th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
 									Title
 								</th>
-								<th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+								<th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
 									Created
 								</th>
-								<th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+								<th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
 									Updated
 								</th>
-								<th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+								<th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
 									Status
 								</th>
-								<th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+								<th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
 								</th>
 							</tr>
 						</thead>
 						<tbody>
 							@foreach ($posts as $post)
 								<tr>
-									<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
 										<p class="text-gray-900 whitespace-no-wrap">
 											{{ Str::limit($post->title, 100) }}
 										</p>
 									</td>
-									<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
 										<p class="text-gray-900 whitespace-no-wrap">
 											{{ $post->created_at->diffForHumans() }}
 										</p>
 									</td>
-									<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
 										<p class="text-gray-900 whitespace-no-wrap">
 											{{ $post->updated_at->diffForHumans() }}
 										</p>
 									</td>
-									<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+									<td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
 										@if($post->active)
-											<span class="inline-flex items-center justify-center px-2 py-1 text-xs leading-none text-green-900 bg-green-200 rounded-full capitalize">
-												Active
+											<span class="inline-flex items-center justify-center px-2 py-1 text-xs leading-none text-green-900 capitalize bg-green-200 rounded-full">
+												Published
 											</span>
 										@else
-											<span class="inline-flex items-center justify-center px-2 py-1 text-xs leading-none text-gray-900 bg-gray-200 rounded-full capitalize">
-												Hidden
+											<span class="inline-flex items-center justify-center px-2 py-1 text-xs leading-none text-gray-900 capitalize bg-gray-200 rounded-full">
+												Draft
+											</span>
+										@endif
+										@if($post->sticky)
+											<span class="inline-flex items-center justify-center px-2 py-1 text-xs leading-none text-red-900 capitalize bg-red-200 rounded-full">
+												<svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+													<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" />
+												</svg>Sticky
 											</span>
 										@endif
 									</td>
-									<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-										<a href="{{ route('blog.show', $post->uuid) }}" class="py-1 px-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg mr-1">
-											<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+									<td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+										<a href="{{ route('blog.show', $post->uuid) }}" class="w-full px-2 py-1 mr-1 text-sm font-semibold text-center text-white transition duration-200 ease-in bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2">
+											<svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
 												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
 											</svg>
 											Show
 										</a>
-										<a href="{{ route('posts.edit', $post) }}" class="py-1 px-2 bg-yellow-600 hover:bg-yellow-700 focus:ring-yellow-500 focus:ring-offset-yellow-200 text-white w-full transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg mr-1">
-											<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+										<a href="{{ route('posts.edit', $post) }}" class="w-full px-2 py-1 mr-1 text-sm font-semibold text-center text-white transition duration-200 ease-in bg-yellow-600 rounded-lg shadow-md hover:bg-yellow-700 focus:ring-yellow-500 focus:ring-offset-yellow-200 focus:outline-none focus:ring-2 focus:ring-offset-2">
+											<svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
 											  </svg>
 											Edit
 										</a>
-										<a href="#" class="py-1 px-2 bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg mr-1">
-											<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+										<a href="#" class="w-full px-2 py-1 mr-1 text-sm font-semibold text-center text-white transition duration-200 ease-in bg-red-600 rounded-lg shadow-md hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2">
+											<svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
 											  </svg>
 											Delete

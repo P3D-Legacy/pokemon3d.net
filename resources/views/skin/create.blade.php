88ee7794e6ac5e230e66c43e51c1e6fe11426948
Daniel S. Billing <daniel.billing@intility.no>
2021-12-07 22:53:58 +0100

    Updated styling for forms

--- resources/views/skin/create.blade.php
+++ resources/views/skin/create.blade.php
@@ -1,50 +1,51 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{'Create Skin'}}
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            {{ __('Create Skin') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <form role="form" action="{{ route('skin-store') }}" method="post" enctype="multipart/form-data">
-                @csrf
-                <div class="mb-3">
-                    <label for="formName" class="text-gray-700">Name</label>
-                    <input class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" type="text" id="formName" name="name" value="{{ old('name') ? old('name') : '' }}">
-                    @error('name')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8"><div class="w-full m-auto overflow-hidden rounded-lg shadow-lg h-90">
+            <div class="block w-full h-full">
+                <div class="w-full p-4 bg-white dark:bg-gray-900">
+                    <form role="form" action="{{ route('skin-store') }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3">
+                            <x-jet-label for="name" value="{{ __('Name') }}" />
+                            <x-jet-input id="name" name="name" type="text" class="block w-full mt-1" value="{{ old('name') ? old('name') : '' }}" autocomplete="name" />
+                            <x-jet-input-error for="name" class="mt-2" />
+                        </div>
+                        <div class="mb-3">
+                            <x-jet-label for="image" value="{{ __('Select image file') }}" />
+                            <input class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" type="file" id="formFile" name="image">
+                            <x-jet-input-error for="image" class="mt-2" />
+                        </div>
+                        <div class="mt-6 mb-3">
+                            <label for="public" class="flex items-center">
+                                <x-jet-checkbox id="public" name="public" />
+                                <span class="ml-2 text-sm text-gray-600 dark:text-gray-300">Public <span class="ml-2 text-sm text-gray-500">Other users will be able to see this skin</span></span>
+                            </label>
+                            <x-jet-input-error for="public" class="mt-2" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="rules" class="flex items-center">
+                                <x-jet-checkbox id="rules" name="rules" />
+                                <span class="ml-2 text-sm text-gray-600 dark:text-gray-300"><strong>I accept and understand the rules</strong> for uploading a custom skin. <span class="my-4 text-sm text-gray-500">Read the rules on the <a href="{{ route('skin-home') }}" class="text-green-500">skin home page</a>.</span></span>
+                            </label>
+                            <x-jet-input-error for="rules" class="mt-2" />
+                        </div>
+                        <x-jet-button>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
+                            </svg>
+                            Upload
+                        </x-jet-button>
+                    </form>
                 </div>
-                <div class="mb-3">
-                    <label for="formFile" class="text-gray-700">Select image file</label>
-                    <input class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" type="file" id="formFile" name="image">
-                    @error('image')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="mt-6 mb-3">
-                    <input class="text-green-600 border-gray-300 rounded shadow-sm focus:border-green-300 focus:ring focus:ring-offset-0 focus:ring-green-200 focus:ring-opacity-50" type="checkbox" id="checkPublic" name="public" @if (old('public')) {{'checked'}} @endif>
-                    <label class="text-gray-700" for="checkPublic">Public <span class="ml-2 text-sm text-gray-500">Other users will be able to see this skin</span></label>
-                    @error('public')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <input class="text-green-600 border-gray-300 rounded shadow-sm focus:border-green-300 focus:ring focus:ring-offset-0 focus:ring-green-200 focus:ring-opacity-50" type="checkbox" id="checkRules" name="rules">
-                    <label class="text-gray-700" for="checkRules"><strong>I accept and understand the rules</strong> for uploading a custom skin</label>
-                    <p class="my-4 text-sm text-center text-gray-500">Read the rules on the <a href="{{ route('skin-home') }}" class="text-green-500">skin home page</a>.</p>
-                    @error('rules')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                <button type="submit" class="flex items-center justify-center w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-green-600 rounded-lg shadow-md hover:bg-green-700 focus:ring-green-500 focus:ring-offset-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
-                    </svg>
-                    Upload
-                </button>
-            </form>
+            </div>
+        </div>
+            
         </div>
     </div>
 </x-app-layout>
--- resources/views/skin/edit.blade.php
+++ resources/views/skin/edit.blade.php
@@ -1,35 +1,35 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{'Edit Skin'}}
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            {{ __('Edit Skin')}}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <form role="form" action="{{ route('skin-update', $skin->uuid) }}" method="post">
-                <div class="mb-3">
-                    <label for="formName" class="text-gray-700">Name</label>
-                    <input class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" type="text" id="formName" name="name" value="{{ old('name') ? old('name') : $skin->name }}">
-                    @error('name')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8"><div class="w-full m-auto overflow-hidden rounded-lg shadow-lg h-90">
+            <div class="block w-full h-full">
+                <div class="w-full p-4 bg-white dark:bg-gray-900">
+                    <form role="form" action="{{ route('skin-update', $skin->uuid) }}" method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <x-jet-label for="name" value="{{ __('Name') }}" />
+                            <x-jet-input id="name" name="name" type="text" class="block w-full mt-1" value="{{ old('name') ? old('name') : $skin->name }}" autocomplete="name" />
+                            <x-jet-input-error for="name" class="mt-2" />
+                        </div>
+                        <div class="mt-6 mb-3">
+                            <<input class="text-green-600 border-gray-300 rounded shadow-sm focus:border-green-300 focus:ring focus:ring-offset-0 focus:ring-green-200 focus:ring-opacity-50" type="checkbox" id="checkPublic" name="public" @if (old('public') ?? $skin->public) {{'checked'}} @endif>
+                            <label class="text-gray-700" for="checkPublic">Public <span class="ml-2 text-sm text-gray-500">Other users will be able to see this skin</span></label>
+                            <x-jet-input-error for="public" class="mt-2" />
+                        </div>
+                        <x-jet-button>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+                            </svg>
+                            Update
+                        </x-jet-button>
+                    </form>
                 </div>
-                <div class="mb-3">
-                    <input class="text-green-600 border-gray-300 rounded shadow-sm focus:border-green-300 focus:ring focus:ring-offset-0 focus:ring-green-200 focus:ring-opacity-50" type="checkbox" id="checkPublic" name="public" @if (old('public') ?? $skin->public) {{'checked'}} @endif>
-                    <label class="text-gray-700" for="checkPublic">Public <span class="ml-2 text-sm text-gray-500">Other users will be able to see this skin</span></label>
-                    @error('public')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                @csrf
-                <button type="submit" class="flex items-center justify-center w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-green-600 rounded-lg shadow-md hover:bg-green-700 focus:ring-green-500 focus:ring-offset-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
-                    </svg>
-                    Update
-                </button>
-            </form>
+            </div>
         </div>
     </div>
 </x-app-layout>

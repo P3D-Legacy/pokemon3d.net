390cfea8267b6fb60a8394728d7bdd56e6f3f93c
Daniel S. Billing <daniel.billing@intility.no>
2021-12-07 23:16:11 +0100

    Published -> Draft

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -28,11 +28,11 @@
                                 @enderror
 							</div>
 							<div class="w-full px-3 mb-6 md:w-1/5 md:mb-0">
-								<label for="active" class="block mb-1 text-gray-700">Published?</label>
+								<label for="active" class="block mb-1 text-gray-700">Draft?</label>
 								<div class="relative inline-block w-full text-gray-700">
 									<select class="w-full h-10 pl-3 pr-6 text-base placeholder-gray-600 border rounded-lg appearance-none focus:shadow-outline" id="active" name="active">
-										<option value="0" {{ !$post->active ? 'selected' : '' }}>No</option>
-										<option value="1" {{ $post->active ? 'selected' : '' }}>Yes</option>
+										<option value="0" {{ !$post->active ? 'selected' : '' }}>Yes</option>
+										<option value="1" {{ $post->active ? 'selected' : '' }}>No</option>
 									</select>
 									<div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
 										<svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>

02e09522b122ace9251623319376d4835b9b4da7
Daniel S. Billing <daniel.billing@intility.no>
2021-12-10 20:21:52 +0100

    Renamed profile edit file and moved function to route file

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ __('Profile') }}
+            {{ __('Edit Profile') }}
         </h2>
     </x-slot>
 

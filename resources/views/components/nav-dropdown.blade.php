502abd87041b1c0ec3e0c9fd951391b461ceaf1f
Daniel S. Billing <daniel.billing@intility.no>
2021-12-10 18:24:17 +0100

    Updated nav-dropdown styling for dark mode

--- resources/views/components/nav-dropdown.blade.php
+++ resources/views/components/nav-dropdown.blade.php
@@ -28,7 +28,7 @@ switch ($width) {
 }
 
 $classes = ($active ?? false)
-            ? 'cursor-pointer relative inline-flex items-center px-1 pt-1 border-b-2 border-green-600 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-green-800 transition'
+            ? 'cursor-pointer relative inline-flex items-center px-1 pt-1 border-b-2 border-green-600 text-sm font-medium leading-5 text-black dark:text-white focus:outline-none focus:border-green-800 transition'
             : 'cursor-pointer relative inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition';
 
 @endphp

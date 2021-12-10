8c64ebf6bb67ded1c76887c14ef6d7f856ee1cd2
Daniel S. Billing <daniel.billing@intility.no>
2021-12-10 18:31:06 +0100

    Updated styling for likes on skin card

--- resources/views/skin/component/card.blade.php
+++ resources/views/skin/component/card.blade.php
@@ -18,7 +18,7 @@
             Uploaded: {{ $skin->created_at->diffForHumans() }}<br>
             File size: {{ Storage::disk('skin')->exists($skin->path()) ? \ByteUnits\Binary::bytes(Storage::disk('skin')->size($skin->path()))->format() : 'N/A' }}
         </p>
-        <div class="flex mt-2 text-sm item-center">
+        <div class="flex mt-2 text-sm item-center text-black dark:text-white">
             <p>
                 <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />

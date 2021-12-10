d52155f780d8d665b8a921bccd3d2e14c6168852
Daniel S. Billing <daniel.billing@intility.no>
2021-12-10 21:15:44 +0100

    Removed header from profile

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,9 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ __('Show Profile') }}
-        </h2>
-    </x-slot>
 
     <div>
         <div class="py-10 mx-auto max-w-7xl sm:px-6 lg:px-8">

db88d7673193b447f553a503afc13c8e7407c636
Daniel S. Billing <daniel.billing@intility.no>
2021-12-11 13:11:52 +0100

    Updated achievement styling

--- resources/views/components/achievement.blade.php
+++ resources/views/components/achievement.blade.php
@@ -1,8 +1,8 @@
 <div>
-    <span class="inline-flex items-center p-2 text-sm font-medium text-gray-400 bg-gray-100 rounded-full tooltip">
+    <span class="inline-flex items-center p-1 text-sm font-medium text-gray-400 bg-gray-100 border border-white rounded-full shadow-sm dark:border-gray-800 dark:bg-black shadow-white/25 tooltip">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
         </svg>
-        <span class="p-3 -mt-6 text-gray-900 bg-blue-200 rounded tooltip-text">{{ $achievement->description }}</span>
+        <span class="p-3 -mt-8 text-gray-900 bg-green-200 rounded tooltip-text">{{ $achievement->description }} - {{ trans_choice('Point|Points', $achievement->points)}}: {{ $achievement->points }}</span>
     </span>
 </div>

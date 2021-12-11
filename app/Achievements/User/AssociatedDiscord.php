24b033645918fe141a66ea880939f967e14179bd
Daniel S. Billing <daniel.billing@intility.no>
2021-12-11 13:11:02 +0100

    Update AssociatedDiscord.php

--- app/Achievements/User/AssociatedDiscord.php
+++ app/Achievements/User/AssociatedDiscord.php
@@ -21,4 +21,9 @@ class AssociatedDiscord extends Achievement
      * A small description for the achievement
      */
     public $description = 'User associated their account with a Discord';
+
+    /*
+     * The amount of "points" this user need to obtain in order to complete this achievement
+     */
+    public $points = 1;
 }

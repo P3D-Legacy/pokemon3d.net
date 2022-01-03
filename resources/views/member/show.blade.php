e103912cf8a789ad9ca657344b2741bdbaa43931
Daniel S. Billing <daniel.billing@intility.no>
2022-01-03 23:51:24 +0100

    Added Facebook account to profile

--- resources/views/member/show.blade.php
+++ resources/views/member/show.blade.php
@@ -128,6 +128,24 @@
                                         </div>
                                     </div>
                                 @endif
+                                @if($user->facebook)
+                                    <div class="pt-2 mt-2 border-t border-slate-200 dark:border-slate-700">
+                                        <svg class="inline-block w-auto h-10 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14222 14222">
+                                            <g>
+                                                <path d="M14222 7111c0,-3927 -3184,-7111 -7111,-7111 -3927,0 -7111,3184 -7111,7111 0,3549 2600,6491 6000,7025l0 -4969 -1806 0 0 -2056 1806 0 0 -1567c0,-1782 1062,-2767 2686,-2767 778,0 1592,139 1592,139l0 1750 -897 0c-883,0 -1159,548 -1159,1111l0 1334 1972 0 -315 2056 -1657 0 0 4969c3400,-533 6000,-3475 6000,-7025z" fill="currentColor"/>
+                                                <path d="M9879 9167l315 -2056 -1972 0 0 -1334c0,-562 275,-1111 1159,-1111l897 0 0 -1750c0,0 -814,-139 -1592,-139 -1624,0 -2686,984 -2686,2767l0 1567 -1806 0 0 2056 1806 0 0 4969c362,57 733,86 1111,86 378,0 749,-30 1111,-86l0 -4969 1657 0z" fill="none"/>
+                                            </g>
+                                        </svg> Facebook
+                                        <div class="flex mb-5 text-gray-600 bg-white rounded shadow dark:text-gray-200 dark:bg-black w-max">
+                                            <div class="self-center p-2 pr-1">
+                                                <img data="picture" class="w-12 h-12 rounded-full" src="{{ $user->facebook->avatar }}" alt="{{ $user->facebook->name }}" />
+                                            </div>
+                                            <div class="self-center w-64 p-2">
+                                                {{ $user->facebook->name }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
                             </div>
                             <div x-show="activeTab===3">
                                 @if($user->gamejolt)

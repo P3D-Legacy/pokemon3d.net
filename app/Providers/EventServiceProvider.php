7a36e20167d59b4b58c1d541f02fbe34205ff5af
Daniel S. Billing <daniel.billing@intility.no>
2022-01-04 16:59:59 +0100

    Installed socialiteproviders/twitch

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            \SocialiteProviders\Twitch\TwitchExtendSocialite::class.'@handle',
+        ],
     ];
 
     /**

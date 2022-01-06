7c6f334927909aaca5093fbe549529eb8399cdcc
Daniel S. Billing <daniel.billing@intility.no>
2022-01-06 22:51:55 +0100

    Updated header title styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,7 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow dark:bg-black">
-                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div class="px-4 py-6 mx-auto text-gray-800 max-w-7xl sm:px-6 lg:px-8 dark:text-gray-200">
                         {{ $header }}
                     </div>
                 </header>

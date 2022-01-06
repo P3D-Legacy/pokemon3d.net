e29bc0e87f03e9f64d8c96fc8981e5e83dd53c15
Daniel S. Billing <daniel.billing@intility.no>
2022-01-06 22:36:01 +0100

    Removed count and added styling

--- resources/views/livewire/server/server-card-list.blade.php
+++ resources/views/livewire/server/server-card-list.blade.php
@@ -1,8 +1,7 @@
 <div class="flex flex-col">
     <div class="-my-2 sm:-mx-6 lg:-mx-8">
-        <p class="py-4 text-center text-gray-300 dark:text-gray-500">{{ __('All servers are updated every hour.') }}</p>
-        <p class="text-white" wire:click="update">{{$this->servers->count()}}</p>
-        <div class="grid grid-cols-3 grid-rows-3 gap-4 grid-flow-rows">
+        <p class="py-4 text-center text-gray-300 dark:text-gray-500">{{ __('All server statuses are updated every hour.') }}</p>
+        <div class="grid grid-cols-3 grid-rows-3 gap-4 p-10 grid-flow-rows">
             @foreach ($this->servers as $server)
                 @livewire('server.server-card', ['server' => $server])
             @endforeach

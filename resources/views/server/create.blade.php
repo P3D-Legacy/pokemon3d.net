44c67436d12688c38dd1b5d238088208719fcf56
Daniel S. Billing <daniel.billing@intility.no>
2022-01-06 22:58:19 +0100

    Updated styling

--- resources/views/server/create.blade.php
+++ resources/views/server/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+        <h2 class="text-xl font-semibold leading-tight">
             {{ __('Server: Create') }}
         </h2>
     </x-slot>

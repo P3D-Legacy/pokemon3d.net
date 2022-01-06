22859da8e46ee2fc1ce715652b941601019813e8
Daniel S. Billing <daniel.billing@intility.no>
2022-01-06 21:53:31 +0100

    Added stats permission

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -37,6 +37,8 @@ class PermissionSeeder extends Seeder
         $p12 = Permission::firstOrCreate(['name' => 'tags.update']);
         $p13 = Permission::firstOrCreate(['name' => 'tags.destroy']);
 
+        $p14 = Permission::firstOrCreate(['name' => 'stats']);
+
         // Super Admin permissions
         $r1->givePermissionTo($p1->name);
         $r1->givePermissionTo($p2->name);
@@ -51,6 +53,7 @@ class PermissionSeeder extends Seeder
         $r1->givePermissionTo($p11->name);
         $r1->givePermissionTo($p12->name);
         $r1->givePermissionTo($p13->name);
+        $r1->givePermissionTo($p14->name);
 
         // Admin permissions
         $r2->givePermissionTo($p2->name);
@@ -63,6 +66,7 @@ class PermissionSeeder extends Seeder
         $r2->givePermissionTo($p11->name);
         $r2->givePermissionTo($p12->name);
         $r2->givePermissionTo($p13->name);
+        $r2->givePermissionTo($p14->name);
 
         // Moderator permissions
         $r3->givePermissionTo($p8->name);

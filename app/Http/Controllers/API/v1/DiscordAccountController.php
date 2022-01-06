13e9c97f966724790e31af4c7ef497f9cb382933
Daniel S. Billing <daniel.billing@intility.no>
2022-01-06 19:17:32 +0100

    Added GET API endpoint for getting Discord account ID #87

---
+++ app/Http/Controllers/API/v1/DiscordAccountController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use Illuminate\Http\Request;
+use App\Models\DiscordAccount;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\API\v1\DiscordAccountResource;
+
+
+/**
+ * @group Discord Account
+ *
+ * APIs for getting Discord accounts.
+ */
+class DiscordAccountController extends Controller
+{
+    /**
+     * Display the specified resource.
+     *
+     * @urlParam id int required The ID of the Discord Account.
+     * 
+     * @response {
+     *    "data": [
+     *        {
+     *            "id": 1,
+     *            "uuid": "1830ef92-b58b-4671-9096-2b7741c0b0d8",
+     *            "id": 1234567890,
+     *            "username": "DanielRTRD",
+     *            "discriminator": 9659,
+     *            "verified_at": "2021-01-01T17:57:10.000000Z",
+     *            "created_at": "2021-01-01T17:57:10.000000Z",
+     *            "updated_at": "2021-01-01T17:57:10.000000Z",
+     *            "deleted_at": null
+     *        },
+     *    ]
+     * }
+     */
+    public function show(Request $request, $id)
+    {
+        if (!$request->user()->tokenCan('read')) {
+            return response()->json([
+                'error' => 'Token does not have access!',
+            ]);
+        }
+        $gja = DiscordAccount::with(['user.roles.permissions'])->where('id', $id)->firstOrFail();
+        return new DiscordAccountResource($gja);
+    }
+}

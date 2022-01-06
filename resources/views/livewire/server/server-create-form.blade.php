1746aea1839a276a16ebf985330eb58719ad156a
Daniel S. Billing <daniel.billing@intility.no>
2022-01-06 22:35:36 +0100

    Updated styling

--- resources/views/livewire/server/server-create-form.blade.php
+++ resources/views/livewire/server/server-create-form.blade.php
@@ -6,7 +6,7 @@
 
         <x-slot name="description">
             {{ __('Do you want your server to be listed for other players to see?') }}
-            <div class="px-4 py-3 my-3 text-sm leading-normal text-blue-600 bg-blue-100 rounded-lg" role="alert">
+            <div class="px-4 py-3 my-3 text-sm leading-normal text-blue-600 bg-blue-100 border rounded-lg border-blue-600/25 dark:text-blue-100 dark:bg-blue-600 dark:border-blue-100/25" role="alert">
                 <p>
                     <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -37,8 +37,8 @@
 
             <div class="col-span-6">
                 <x-jet-label for="description" value="{{ __('Description') }}" />
-                <textarea id="description" wire:model.defer="description" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 dark:bg-black dark:text-white dark:border-gray-900"></textarea>
-                <x-jet-input-error for="description" class="mt-2" />
+                <textarea id="description" wire:model.defer="description" class="block w-full mt-1 px-3 py-1.5 border-gray-300 rounded-md shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 dark:bg-black dark:text-white dark:border-gray-900" placeholder="This is a server for me and my friends."></textarea>
+                <x-jet-input-error for="description" class="mt-2"/>
             </div>
 
         </x-slot>

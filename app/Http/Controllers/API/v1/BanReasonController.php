d0e1054a8fab7eacb477da789eb8e62a59349556
Daniel S. Billing <daniel.billing@intility.no>
2022-01-08 23:30:52 +0100

    Updated API endpoint responses

--- app/Http/Controllers/API/v1/BanReasonController.php
+++ app/Http/Controllers/API/v1/BanReasonController.php
@@ -21,8 +21,28 @@ class BanReasonController extends Controller
 
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * 
+     * @response {
+     *    "data": [
+     *        {
+     *            "uuid": "1830ef92-b58b-4671-9096-2b7741c0b0d8",
+     *            "name": "Abusing in-game glitches",
+     *            "user_id": 1,
+     *            "created_at": "2021-01-01T17:57:10.000000Z",
+     *            "updated_at": "2021-01-01T17:57:10.000000Z",
+     *            "deleted_at": null
+     *        },
+     *        {
+     *            "id": 2,
+     *            "uuid": "ba3900a8-bc82-43c6-9d4f-1760205f95f6",
+     *            "name": "General hacking or cheating",
+     *            "user_id": 1,
+     *            "created_at": "2022-01-04T18:49:46.000000Z",
+     *            "updated_at": "2022-01-04T18:49:46.000000Z",
+     *            "deleted_at": null
+     *        },
+     *    ]
+     * }
      */
     public function index(Request $request)
     {
--- app/Http/Controllers/API/v1/UserController.php
+++ app/Http/Controllers/API/v1/UserController.php
@@ -20,11 +20,57 @@ class UserController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * Display the specified resource.
+    *
+    * @urlParam id int required The ID of the user.
+    * 
+    * @response {
+    *        "data": {
+    *           "id": 1,
+    *           "name": "Daniel S. Billing",
+    *           "email": "daniel@rtrd.no",
+    *           "username": "DanielRTRD",
+    *           "about": null,
+    *           "gender": 1,
+    *           "location": null,
+    *           "birthdate": null,
+    *           "email_verified_at": "2021-12-21T20:59:59.000000Z",
+    *           "current_team_id": null,
+    *           "profile_photo_path": null,
+    *           "created_at": "2021-12-21T20:58:39.000000Z",
+    *           "updated_at": "2022-01-01T22:15:21.000000Z",
+    *           "last_active_at": "2022-01-01T22:15:21.000000Z",
+    *           "profile_photo_url": "https://ui-avatars.com/api/?name=Daniel+S.+Billing&color=7F9CF5&background=EBF4FF",
+    *           "roles": [
+    *                {
+    *                    "id": 1,
+    *                    "name": "super-admin",
+    *                    "guard_name": "web",
+    *                    "created_at": "2022-01-01T22:49:45.000000Z",
+    *                    "updated_at": "2022-01-01T22:49:45.000000Z",
+    *                    "pivot": {
+    *                        "model_id": 1,
+    *                        "role_id": 1,
+    *                        "model_type": "App\\Models\\User"
+    *                    },
+    *                    "permissions": [
+    *                        {
+    *                            "id": 1,
+    *                            "name": "manage.users",
+    *                            "guard_name": "web",
+    *                            "created_at": "2022-01-01T22:49:45.000000Z",
+    *                            "updated_at": "2022-01-01T22:49:45.000000Z",
+    *                            "pivot": {
+    *                                "role_id": 1,
+    *                                "permission_id": 1
+    *                            }
+    *                        }
+    *                    ]
+    *                }
+    *            ],
+    *        },
+    *   }
+    */
     public function show(Request $request, $id)
     {
         if (!$request->user()->tokenCan('read')) {

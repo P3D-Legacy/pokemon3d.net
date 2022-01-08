f4952ebfb2ec05c3135ca6185002d2b7356f0064
Daniel S. Billing <daniel.billing@intility.no>
2022-01-08 21:56:09 +0100

    Fixed styling for servers on smaller screens

--- resources/views/livewire/server/server-card-list.blade.php
+++ resources/views/livewire/server/server-card-list.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col">
     <div class="-my-2 sm:-mx-6 lg:-mx-8">
         <p class="py-4 text-center text-gray-300 dark:text-gray-600">{{ __('All server statuses are updated every hour.') }}</p>
-        <div class="grid grid-cols-3 grid-rows-3 gap-4 p-10 grid-flow-rows">
+        <div class="grid grid-cols-1 gap-4 sm:p-10 sm:grid-cols-2 md:grid-cols-3 auto-rows-auto grid-flow-rows">
             @foreach ($this->servers as $server)
                 @livewire('server.server-card', ['server' => $server])
             @endforeach

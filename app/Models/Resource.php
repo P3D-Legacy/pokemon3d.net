f268a68a36c8071edabef31b2be8bf14e3941903
Daniel S. Billing <daniel.billing@intility.no>
2022-01-09 20:45:11 +0100

    Create Resource.php

---
+++ app/Models/Resource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Tags\HasTags;
+use Illuminate\Database\Eloquent\Model;
+use Overtrue\LaravelLike\Traits\Likeable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use CyrildeWit\EloquentViewable\InteractsWithViews;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
+class Resource extends Model
+{
+    use HasFactory;
+    use InteractsWithViews;
+    use Likeable;
+    use SoftDeletes;
+    use HasTags;
+    use Uuid;
+
+    protected $primaryKey = 'uuid';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'description',
+        'user_id',
+    ];
+
+    /** 
+     * The attributes that should be hidden
+     * 
+     * @var array
+     */
+    protected $hidden = [
+        'aid',
+    ];
+
+    /**
+     * Get the user that made this post.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}

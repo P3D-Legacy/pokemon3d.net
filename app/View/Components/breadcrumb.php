cddcb8f2bb109a2d6e85b0eded929676747484e8
Daniel S. Billing <daniel.billing@intility.no>
2022-01-10 22:15:50 +0100

    Added modular breadcrumbs

---
+++ app/View/Components/breadcrumb.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class breadcrumb extends Component
+{
+    public $breadcrumbs;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($breadcrumbs)
+    {
+        $this->breadcrumbs = $breadcrumbs;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.breadcrumb', ['breadcrumbs' => $this->breadcrumbs]);
+    }
+}

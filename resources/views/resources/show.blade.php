7beb63616d83f4cdc91fc9851b98356d3dc0da47
Daniel S. Billing <daniel.billing@intility.no>
2022-01-10 21:29:13 +0100

    Added edit button to resource

--- resources/views/resources/show.blade.php
+++ resources/views/resources/show.blade.php
@@ -27,11 +27,23 @@
                             Download
                         </button>
                     --}}
-                    <button class="px-2 py-1 text-gray-100 transition-colors duration-150 bg-gray-600 rounded-md focus:shadow-outline hover:bg-gray-700">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
-                        </svg>
-                    </button>
+                    @if(auth()->user()->id == $resource->user_id)
+                        <div x-data="{ dropdownOpen: false }" class="" x-cloak>
+                            <button @click="dropdownOpen = !dropdownOpen" class="px-2 py-1 text-gray-100 transition-colors duration-150 bg-gray-600 rounded-md focus:shadow-outline hover:bg-gray-700">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                                </svg>
+                            </button>
+                            <div x-show="dropdownOpen" @click.away="dropdownOpen = false" class="absolute right-0 z-40 w-48 py-2 mt-2 bg-white border border-gray-200 rounded-md shadow-md dark:bg-gray-900 dark:border-gray-800">
+                                <a href="{{ route('resource.edit', $resource) }}" class="block px-4 py-2 text-sm text-gray-700 capitalize dark:text-gray-300 hover:bg-green-700 hover:text-white">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                    </svg>
+                                    Edit
+                                </a>
+                            </div>
+                        </div>
+                    @endif
                 </div>
             </div>
             <div class="w-full p-4 bg-white rounded-lg shadow-lg dark:bg-gray-900 dark:shadow-gray-700">

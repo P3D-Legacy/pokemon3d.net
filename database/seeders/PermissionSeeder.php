4c80b86bb105b44a6b87cb3efa727514109bfd3b
Daniel S. Billing <daniel.billing@intility.no>
2022-01-12 22:28:58 +0100

    Update PermissionSeeder.php

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -37,6 +37,10 @@ class PermissionSeeder extends Seeder
         $p12 = Permission::firstOrCreate(['name' => 'tags.update']);
         $p13 = Permission::firstOrCreate(['name' => 'tags.destroy']);
 
+        $p14 = Permission::firstOrCreate(['name' => 'categories.create']);
+        $p15 = Permission::firstOrCreate(['name' => 'categories.update']);
+        $p16 = Permission::firstOrCreate(['name' => 'categories.destroy']);
+
         // Super Admin permissions
         $r1->givePermissionTo($p1->name);
         $r1->givePermissionTo($p2->name);
@@ -51,6 +55,9 @@ class PermissionSeeder extends Seeder
         $r1->givePermissionTo($p11->name);
         $r1->givePermissionTo($p12->name);
         $r1->givePermissionTo($p13->name);
+        $r1->givePermissionTo($p14->name);
+        $r1->givePermissionTo($p15->name);
+        $r1->givePermissionTo($p16->name);
 
         // Admin permissions
         $r2->givePermissionTo($p2->name);

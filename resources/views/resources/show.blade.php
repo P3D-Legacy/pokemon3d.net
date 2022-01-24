f04c5222af37cc0273813aab8548edab123950fd
Daniel S. Billing <daniel.billing@intility.no>
2022-01-24 22:54:49 +0100

    Removed header for resource page

--- resources/views/resources/show.blade.php
+++ resources/views/resources/show.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight">
-            {{ __('Resources') }}
-        </h2>
-    </x-slot>
-
     <div>
         @livewire('resource.resource-show', ['resource' => $resource])
     </div>

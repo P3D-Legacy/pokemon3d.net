ef444e7f06625bbdbaf693f0cc3b4fef051c2f25
Daniel S. Billing <daniel.billing@intility.no>
2022-02-02 17:58:21 +0100

    WIP file upload

--- app/Http/Livewire/Resource/UpdateCreate.php
+++ app/Http/Livewire/Resource/UpdateCreate.php
@@ -2,12 +2,48 @@
 
 namespace App\Http\Livewire\Resource;
 
-use LivewireUI\Modal\ModalComponent;
+use App\Models\Resource;
 use Livewire\WithFileUploads;
+use App\Models\ResourceUpdate;
+use LivewireUI\Modal\ModalComponent;
 
 class UpdateCreate extends ModalComponent
 {
     use WithFileUploads;
+
+    public ResourceUpdate $resourceUpdate;
+    public int|Resource $resource;
+    public $file;
+    public $version;
+    public $description;
+
+    protected array $rules = [
+        'file' => ['required', 'file', 'mimes:zip']
+    ];
+
+    public function mount(int|Resource $resource)
+    {
+        $this->resource = $resource;
+        $this->version = "";
+        $this->description = "";
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->resourceUpdate = ResourceUpdate::create([
+            'title' => $this->version,
+            'body' => $this->description,
+            'resource_id' => $this->resource,
+            'game_version_id' => 1,
+        ]);
+
+        $this->resourceUpdate->clearMediaCollection('resource_update_file');
+        $this->resourceUpdate->addMedia($this->file->getRealPath())->toMediaCollection('resource_update_file');
+
+        return redirect()->route('posts.index');
+    }
 
     public function render()
     {

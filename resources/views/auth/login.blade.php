6c5ea23d4add13acacbfc6d1c35a201de881517d
Daniel S. Billing <daniel.billing@intility.no>
2022-02-09 20:50:41 +0100

    Add dark-green color for gamejolt in dark mode

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -99,7 +99,7 @@
 				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
 			</svg>{{ __('Forum Account') }}
 		</button>
-		<button class="flex items-center justify-center w-full px-4 py-3 mt-2 text-sm font-semibold tracking-widest text-black uppercase transition border border-transparent rounded-md bg-gamejolt-green hover:bg-opacity-70 focus:outline-none focus:border-green-200 focus:ring focus:ring-green-100 disabled:opacity-25" type="button" onclick="toggleModal('gamejolt')">
+		<button class="flex items-center justify-center w-full px-4 py-3 mt-2 text-sm font-semibold tracking-widest text-black uppercase transition border border-transparent rounded-md bg-gamejolt-green dark:bg-gamejolt-dark-green hover:bg-opacity-70 focus:outline-none focus:border-green-200 focus:ring focus:ring-green-100 disabled:opacity-25" type="button" onclick="toggleModal('gamejolt')">
 			<img src="{{ asset('img/gamejolt-logo-light-1x.png') }}">
 		</button>
 		<div class="grid grid-flow-col gap-2 auto-cols-auto">

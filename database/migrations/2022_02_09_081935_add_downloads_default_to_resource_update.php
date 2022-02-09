9b471fa6813881999c210b5f3eb8de816cace3d6
Daniel S. Billing <daniel.billing@intility.no>
2022-02-09 09:22:13 +0100

    Create 2022_02_09_081935_add_downloads_default_to_resource_update.php

---
+++ database/migrations/2022_02_09_081935_add_downloads_default_to_resource_update.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDownloadsDefaultToResourceUpdate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('resource_update', function (Blueprint $table) {
+            $table->unsignedInteger('downloads')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('resource_update', function (Blueprint $table) {
+            $table->unsignedInteger('downloads')->change();
+        });
+    }
+}

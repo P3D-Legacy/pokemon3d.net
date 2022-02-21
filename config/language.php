8712b2375acf816e683cfef6d1a8d0354ddb0ec8
Daniel S. Billing <daniel.billing@intility.no>
2022-02-21 21:26:56 +0100

    Update language.php

--- config/language.php
+++ config/language.php
@@ -135,7 +135,7 @@ return [
     | This options indicates the language allowed languages.
     |
     */
-    'allowed' => ['en', 'cn', 'nl', 'el'],
+    'allowed' => ['en', 'cn', 'nl', 'el', 'es'],
 
     /*
     |--------------------------------------------------------------------------

4e370ba2f61b853f90225b68c44869bb0cdb353a
Daniel S. Billing <daniel.billing@intility.no>
2022-02-22 16:50:45 +0100

    Added default ET lang files

---
+++ resources/lang/et/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset'     => 'Sinu parool on lähtestatud!',
+    'sent'      => 'Oleme saatnud Sulle e-postiga parooli lähtestamise lingi!',
+    'throttled' => 'Palun oodake enne uuesti proovimist.',
+    'token'     => 'See parooli lähtestamise tunnus on vigane.',
+    'user'      => 'Sellise e-postiaadressiga kasutajat ei leitud.',
+];

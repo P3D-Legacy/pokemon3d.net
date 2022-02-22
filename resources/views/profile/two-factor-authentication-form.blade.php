e48121bb7d591b14dcc9c38d512e578f64ca933f
Daniel S. Billing <daniel.billing@intility.no>
2022-02-22 18:47:43 +0100

    Update two-factor-authentication-form.blade.php

--- resources/views/profile/two-factor-authentication-form.blade.php
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -18,7 +18,7 @@
 
         <div class="max-w-xl mt-3 text-sm text-gray-600 dark:text-gray-400">
             <p>
-                {{ __("When two factor authentication is enabled, you will be prompted for a secure, random token during authentication. You may retrieve this token from your phone's Google Authenticator application.") }}
+                {{ __("When two factor authentication is enabled, you will be prompted for a secure, random token during authentication. You may retrieve this token from your phone's Authenticator application.") }}
             </p>
         </div>
 

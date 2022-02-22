17040762e5bbc6b0aeeb329019f1983b0400a46e
Daniel S. Billing <daniel.billing@intility.no>
2022-02-22 16:48:00 +0100

    Updated chinese lang

--- config/language.php
+++ config/language.php
@@ -135,7 +135,7 @@ return [
     | This options indicates the language allowed languages.
     |
     */
-    'allowed' => ['en', 'cn', 'nl', 'el', 'es', 'ru', 'sv', 'et', 'tw'],
+    'allowed' => ['en', 'zh_CN', 'nl', 'el', 'es', 'ru', 'sv', 'et', 'zh_TW'],
 
     /*
     |--------------------------------------------------------------------------

343cc35bf5e2982b99b1af4c060837fc423bcb86
Daniel S. Billing <daniel@kilobyte.no>
2022-02-23 18:22:55 +0100

    Updated translations

--- resources/views/skin/show.blade.php
+++ resources/views/skin/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{'Public Skins: '.$skin->name}}
+            {{ __('Public Skins') . ': ' . $skin->name }}
         </h2>
     </x-slot>
 
@@ -10,8 +10,8 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
 
             @component('components.breadcrumb', ['breadcrumbs' => [
-                ['label' => 'Skins'],
-                ['label' => 'Public'],
+                ['label' => __('Skins')],
+                ['label' => __('Public')],
                 ['label' => $skin->name],
             ]])
             @endcomponent

cde4fdce803a46c738b54a74ceface05c3366caf
Daniel S. Billing <daniel@kilobyte.no>
2022-02-24 15:12:27 +0100

    Update DiscordRole.php

--- app/Models/DiscordRole.php
+++ app/Models/DiscordRole.php
@@ -9,5 +9,31 @@ class DiscordRole extends Model
 {
     use HasFactory;
 
+    protected $primaryKey = 'id';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = ['color', 'hoist', 'id', 'managed', 'mentionable', 'name', 'permissions', 'position'];
+
+    public function accounts()
+    {
+        return $this->belongsToMany(DiscordAccount::class);
+    }
 }

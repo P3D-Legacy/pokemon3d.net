737f755e6720fb7ff7095e09d99cfe6343a97d2c
Daniel S. Billing <daniel@kilobyte.no>
2022-02-24 22:02:26 +0100

    Update review-list.blade.php

--- resources/views/livewire/review/review-list.blade.php
+++ resources/views/livewire/review/review-list.blade.php
@@ -1,20 +1,20 @@
-<div class="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-4 rounded-lg overflow-hidden shadow-lg text-gray-700 border border-gray-200 bg-gray-50 dark:bg-gray-900 dark:border-gray-700 p-4 dark:text-gray-100">
-    <div class="font-bold text-lg my-1">@lang('Overall Game Reviews')</div>
+<div class="col-span-1 p-4 overflow-hidden text-gray-700 border border-gray-200 rounded-lg shadow-lg sm:col-span-2 md:col-span-3 lg:col-span-4 bg-gray-50 dark:bg-gray-900 dark:border-gray-700 dark:text-gray-100">
+    <div class="my-1 text-lg font-bold">@lang('Overall Game Reviews')</div>
     <div class="flex items-center"><x-review-stars :stars="$averageRating" /><span class="ml-2">{{ round($averageRating, 2) }} ({{ $numberOfReviews }})</span></div>
 </div>
 
 @foreach ($reviews as $review)
-    <div class="rounded-lg overflow-hidden shadow-lg bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-4">
-        <div class="flex items-center h-10 w-full">
+    <div class="p-4 overflow-hidden border border-gray-200 rounded-lg shadow-lg bg-gray-50 dark:bg-gray-900 dark:text-gray-100 dark:border-gray-700">
+        <div class="flex items-center w-full h-10">
             <a href="{{ route('member.show', $review->author) }}">
-                <img alt="{{ $review->author->username }}" src="{{ $review->author->profile_photo_url ?? asset('img/TreeLogoSmall.png') }}" class="object-cover w-8 h-8 mx-auto rounded-full inline-flex mr-2"/>
+                <img alt="{{ $review->author->username }}" src="{{ $review->author->profile_photo_url ?? asset('img/TreeLogoSmall.png') }}" class="inline-flex object-cover w-8 h-8 mx-auto mr-2 rounded-full"/>
             </a>
-            <a href="{{ route('member.show', $review->author) }}" class="text-green-400 hover:text-green-500 mr-2 hover:underline">{{ $review->author->username }}</a>
+            <a href="{{ route('member.show', $review->author) }}" class="mr-2 text-green-400 hover:text-green-500 hover:underline">{{ $review->author->username }}</a>
         </div>
         <div>
             <x-review-stars :stars="$review->rating" :size="6" />
-            <div class="font-bold text-lg my-1">{{ $review->model->version }}</div>
-            <p class="text-gray-700 dark:text-gray-200 break-all">
+            <div class="my-1 text-lg font-bold">{{ $review->model->version }}</div>
+            <p class="text-gray-700 break-all dark:text-gray-200">
                 {{ $review->review }}
             </p>
         </div>

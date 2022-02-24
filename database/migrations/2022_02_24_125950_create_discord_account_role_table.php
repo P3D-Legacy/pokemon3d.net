f1aaf13a019ffdb680c87adcf8ace8d1004f99bf
Daniel S. Billing <daniel@kilobyte.no>
2022-02-24 15:11:41 +0100

    Create 2022_02_24_125950_create_discord_account_role_table.php

---
+++ database/migrations/2022_02_24_125950_create_discord_account_role_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscordAccountRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discord_account_discord_role', function (Blueprint $table) {
+            $table->bigInteger('discord_account_id');
+            $table->foreign('discord_account_id')->references('id')->on('discord_accounts')->onDelete('cascade');
+            $table->bigInteger('discord_role_id');
+            $table->foreign('discord_role_id')->references('id')->on('discord_roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discord_account_discord_role');
+    }
+}

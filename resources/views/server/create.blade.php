c71af587ac47f7ccd6818ff66635337447f79b13
Daniel S. Billing <daniel@kilobyte.no>
2022-02-24 12:03:47 +0100

    Removed header

--- resources/views/server/create.blade.php
+++ resources/views/server/create.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight">
-            {{ __('Servers') }}
-        </h2>
-    </x-slot>
-
     <div>
         <div class="max-w-4xl py-10 mx-auto sm:px-6 lg:px-8">
 

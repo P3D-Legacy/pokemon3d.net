7bd00b8b32d89b284fb30485909c2989224c32a4
Daniel S. Billing <daniel@kilobyte.no>
2022-02-24 12:04:08 +0100

    Updated translations for servers

--- resources/views/server/index.blade.php
+++ resources/views/server/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight">
-            Servers <a href="{{ route('server.create') }}" class="px-2 py-1 ml-4 text-sm font-bold text-white bg-green-500 rounded hover:bg-green-700"><svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" /></svg> @lang('Create')</a>
+            @lang('Servers') <a href="{{ route('server.create') }}" class="px-2 py-1 ml-4 text-sm font-bold text-white bg-green-500 rounded hover:bg-green-700"><svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" /></svg> @lang('Create')</a>
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="px-4 py-10 mx-auto max-w-7xl sm:px-6 lg:px-8">
 
             @component('components.breadcrumb', ['breadcrumbs' => [
-                ['label' => 'Servers'],
+                ['label' => __('Servers')],
             ]])
             @endcomponent
 

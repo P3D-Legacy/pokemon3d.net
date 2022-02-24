6382945f4951c6fc7687d990b5ffb18b698bf68f
Daniel S. Billing <daniel@kilobyte.no>
2022-02-24 11:15:24 +0100

    Added read more blog posts link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -231,6 +231,16 @@
                     </a>
                 </div>
             @endforelse
+            @if($posts->count() > 0)
+                <div class="w-full text-xs text-center">
+                    <a href="{{ route('blog.index') }}" class="text-green-500 hover:underline">
+                        @lang('Read more blog posts')
+                        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+                        </svg>
+                    </a>
+                </div>
+            @endif
         </div>
     </section>
 

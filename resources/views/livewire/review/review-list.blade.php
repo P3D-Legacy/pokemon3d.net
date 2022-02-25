82841f42943d3bb048fe99b97e6ff94482558d4b
Daniel S. Billing <daniel@kilobyte.no>
2022-02-25 08:17:18 +0100

    Fixed reviews list not updating

--- resources/views/livewire/review/review-list.blade.php
+++ resources/views/livewire/review/review-list.blade.php
@@ -1,22 +1,26 @@
-<div class="col-span-1 p-4 overflow-hidden text-gray-700 border border-gray-200 rounded-lg shadow-lg sm:col-span-2 md:col-span-3 lg:col-span-4 bg-gray-50 dark:bg-gray-900 dark:border-gray-700 dark:text-gray-100">
-    <div class="my-1 text-lg font-bold">@lang('Overall Game Reviews')</div>
-    <div class="flex items-center"><x-review-stars :stars="$averageRating" /><span class="ml-2">{{ round($averageRating, 2) }} ({{ $numberOfReviews }})</span></div>
-</div>
-
-@foreach ($reviews as $review)
-    <div class="p-4 overflow-hidden border border-gray-200 rounded-lg shadow-lg bg-gray-50 dark:bg-gray-900 dark:text-gray-100 dark:border-gray-700">
-        <div class="flex items-center w-full h-10">
-            <a href="{{ route('member.show', $review->author) }}">
-                <img alt="{{ $review->author->username }}" src="{{ $review->author->profile_photo_url ?? asset('img/TreeLogoSmall.png') }}" class="inline-flex object-cover w-8 h-8 mx-auto mr-2 rounded-full"/>
-            </a>
-            <a href="{{ route('member.show', $review->author) }}" class="mr-2 text-green-400 hover:text-green-500 hover:underline">{{ $review->author->username }}</a>
-        </div>
-        <div>
-            <x-review-stars :stars="$review->rating" :size="6" />
-            <div class="my-1 text-lg font-bold">{{ $review->model->version }}</div>
-            <p class="text-gray-700 break-word dark:text-gray-200">
-                {{ $review->review }}
-            </p>
-        </div>
+<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-cols">
+    
+    <div class="col-span-1 p-4 overflow-hidden text-gray-700 border border-gray-200 rounded-lg shadow-lg sm:col-span-2 md:col-span-3 lg:col-span-4 bg-gray-50 dark:bg-gray-900 dark:border-gray-700 dark:text-gray-100">
+        <div class="my-1 text-lg font-bold">@lang('Overall Game Reviews')</div>
+        <div class="flex items-center"><x-review-stars :stars="$averageRating" /><span class="ml-2">{{ round($averageRating, 2) }} ({{ $numberOfReviews }})</span></div>
     </div>
-@endforeach
+
+    @foreach ($reviews as $review)
+        <div class="p-4 overflow-hidden border border-gray-200 rounded-lg shadow-lg bg-gray-50 dark:bg-gray-900 dark:text-gray-100 dark:border-gray-700">
+            <div class="flex items-center w-full h-10">
+                <a href="{{ route('member.show', $review->author) }}">
+                    <img alt="{{ $review->author->username }}" src="{{ $review->author->profile_photo_url ?? asset('img/TreeLogoSmall.png') }}" class="inline-flex object-cover w-8 h-8 mx-auto mr-2 rounded-full"/>
+                </a>
+                <a href="{{ route('member.show', $review->author) }}" class="mr-2 text-green-400 hover:text-green-500 hover:underline">{{ $review->author->username }}</a>
+            </div>
+            <div>
+                <x-review-stars :stars="$review->rating" :size="6" />
+                <div class="my-1 text-lg font-bold">{{ $review->model->version }}</div>
+                <p class="text-gray-700 break-word dark:text-gray-200">
+                    {{ $review->review }}
+                </p>
+            </div>
+        </div>
+    @endforeach
+    
+</div>
--- resources/views/review/index.blade.php
+++ resources/views/review/index.blade.php
@@ -11,9 +11,8 @@
                 ['url' => null, 'label' => __('Reviews')],
             ]])
             @endcomponent
-            <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-cols">
-                @livewire('review.review-list')
-            </div>
+            
+            @livewire('review.review-list')
         </div>
     </div>
 </x-app-layout>

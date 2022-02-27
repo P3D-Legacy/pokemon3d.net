fd5dd7e247a8288809d8dc25d45a884bd43b8215
Daniel S. Billing <daniel@kilobyte.no>
2022-02-27 17:27:50 +0100

    Filament locale

---
+++ resources/lang/vendor/filament/es/layout.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'direction' => 'ltr',
+
+    'buttons' => [
+
+        'dark_mode' => [
+            'label' => 'Modo oscuro',
+        ],
+
+        'light_mode' => [
+            'label' => 'Modo claro',
+        ],
+
+        'logout' => [
+            'label' => 'Salir',
+        ],
+    ],
+
+];

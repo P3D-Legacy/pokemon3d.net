cc5912609ea5c883b92a1b38a4069722b706152a
Daniel S. Billing <daniel@kilobyte.no>
2022-02-27 18:07:34 +0100

    Add Post

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-newspaper';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\MarkdownEditor::make('body')
+                    ->required(),
+                Forms\Components\Toggle::make('active')
+                    ->required(),
+                Forms\Components\Toggle::make('sticky')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\BooleanColumn::make('active'),
+                Tables\Columns\BooleanColumn::make('sticky'),
+                Tables\Columns\TextColumn::make('user.username'),
+                Tables\Columns\TextColumn::make('published_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/PostResource/Widgets/BlogPostsOverview.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Resources\PostResource\Widgets;
+
+use Filament\Widgets\Widget;
+
+class BlogPostsOverview extends Widget
+{
+    protected static string $view = 'filament.resources.post-resource.widgets.blog-posts-overview';
+}

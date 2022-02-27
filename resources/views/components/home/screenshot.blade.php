3665d061f0c4f6b61b1683be08787bbf9c7aef9a
Daniel S. Billing <daniel@kilobyte.no>
2022-02-27 18:56:13 +0100

    Remove text on screenshot

--- resources/views/components/home/screenshot.blade.php
+++ resources/views/components/home/screenshot.blade.php
@@ -1,7 +1,9 @@
 <div class="relative float-left w-full carousel-item {{ $active ? 'active' : '' }}">
     <img src="{{ asset($path) }}" class="block object-cover" alt="" />
+    {{--
     <div class="absolute hidden text-center carousel-caption md:block">
         <h5 class="text-xl drop-shadow shadow-black dark:shadow-white">{{ $title }}</h5>
         <p>@lang('By'): {{ $author }}</p>
     </div>
+    --}}
 </div>

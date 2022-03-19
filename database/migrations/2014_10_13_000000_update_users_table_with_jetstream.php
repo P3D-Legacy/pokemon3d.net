c6491a51722f9a9199874108bea61556552a86fb
Daniel S. Billing <daniel@kilobyte.no>
2022-03-19 08:43:26 +0100

    Create 2014_10_13_000000_update_users_table_with_jetstream.php

---
+++ database/migrations/2014_10_13_000000_update_users_table_with_jetstream.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasColumn('users', 'current_team_id')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->foreignId('current_team_id')->nullable();
+            });
+        }
+        if (!Schema::hasColumn('users', 'profile_photo_path')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('profile_photo_path', 2048)->nullable();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (!Schema::hasColumn('users', 'current_team_id')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('current_team_id');
+            });
+        }
+        if (!Schema::hasColumn('users', 'profile_photo_path')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('profile_photo_path');
+            });
+        }
+    }
+};

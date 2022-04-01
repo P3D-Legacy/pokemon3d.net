d0d03bdc027ce4f3bb96388033be0f7d51ad5f2b
Daniel S. Billing <daniel@kilobyte.no>
2022-04-01 20:07:20 +0200

    Create 2022_04_01_180428_update_gamejolt_account_trophies.php

---
+++ database/migrations/2022_04_01_180428_update_gamejolt_account_trophies.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('gamejolt_account_trophies', function (Blueprint $table) {
+            $table->bigIncrements('aid')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('gamejolt_account_trophies', function (Blueprint $table) {
+            $table->increments('aid')->change();
+        });
+    }
+};

dee0cab6292de83b3fbfe7361eba4956d8593c77
Daniel S. Billing <daniel@kilobyte.no>
2022-04-03 15:34:22 +0200

    Installed alibayat/laravel-commentable

---
+++ config/laravel-commentable.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Laravel Commentable Package by Ali Bayat.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Eloquent Model
+    |--------------------------------------------------------------------------
+    */
+    
+    'model' => \AliBayat\LaravelCommentable\Comment::class,
+
+];

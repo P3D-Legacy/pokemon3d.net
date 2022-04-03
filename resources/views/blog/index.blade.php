d2515781143dbf21865121876d2715f743d8a629
Daniel S. Billing <daniel@kilobyte.no>
2022-04-03 15:44:08 +0200

    Add comments count

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -33,7 +33,7 @@
 						</p>
 					</div>
 				</div>
-				
+
 			</div>
 
 			<div class="grid w-full grid-cols-1 gap-8 sm:max-w-2xl">
@@ -84,6 +84,10 @@
 												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
 												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
 											</svg> {{ App\Helpers\NumberHelper::nearestK(views($post)->count()) }}
+
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+                                            </svg> {{ App\Helpers\NumberHelper::nearestK($post->commentCount()) }}
 										</p>
 									</div>
 								</div>

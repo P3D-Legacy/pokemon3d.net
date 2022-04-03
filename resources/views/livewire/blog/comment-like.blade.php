8ac5db3b194b59d1ce1114fd72b9cf946e8afc88
Daniel S. Billing <daniel@kilobyte.no>
2022-04-03 17:18:36 +0200

    Update comment-like.blade.php

--- resources/views/livewire/blog/comment-like.blade.php
+++ resources/views/livewire/blog/comment-like.blade.php
@@ -1,4 +1,4 @@
-<span class="inline-flex items-center justify-center px-2 py-1 mr-2 font-bold leading-none uppercase rounded text-xs cursor-pointer {{ ($liked ? 'text-red-500 hover:text-red-400' : 'text-gray-600 dark:text-gray-600 hover:text-red-400') }}" wire:click="like">
+<span class="inline-flex items-center justify-center px-2 py-1 mr-2 font-bold leading-none uppercase rounded text-xs cursor-pointer {{ ($liked ? 'text-red-500 hover:text-red-400' : 'text-gray-600 dark:text-gray-400 hover:text-red-400') }}" wire:click="like">
     @if($liked)
         <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-3 h-3 mr-1" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />

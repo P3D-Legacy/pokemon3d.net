6df22d5f91da61198a04b778422a1ec787e571d5
DanielRTRD <DanielRTRD@users.noreply.github.com>
2022-04-09 15:24:42 +0000

    Prettified Code!

--- config/timezone.php
+++ config/timezone.php
@@ -62,7 +62,16 @@ return [
 
     'lookup' => [
         'server' => ['REMOTE_ADDR'],
-        'headers' => ['HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'HTTP_VIA', 'REMOTE_ADDR'],
+        'headers' => [
+            'HTTP_CLIENT_IP',
+            'HTTP_X_FORWARDED_FOR',
+            'HTTP_X_FORWARDED',
+            'HTTP_X_CLUSTER_CLIENT_IP',
+            'HTTP_FORWARDED_FOR',
+            'HTTP_FORWARDED',
+            'HTTP_VIA',
+            'REMOTE_ADDR',
+        ],
     ],
 
     /*

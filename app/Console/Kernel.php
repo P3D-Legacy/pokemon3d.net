bc1818bead7b6eb6593e9bd49fa99d258897356b
Daniel S. Billing <daniel@kilobyte.no>
2022-05-19 20:25:37 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,9 +2,16 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DiscordRoleSync;
+use App\Console\Commands\DiscordUserRoleSync;
 use App\Console\Commands\NotifyGameUpdate;
+use App\Console\Commands\PingAllServers;
+use App\Console\Commands\SkinUserUpdate;
+use App\Console\Commands\SyncGameVersion;
+use App\Console\Commands\UpdateGamejoltAccountTrophies;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Spatie\Activitylog\CleanActivitylogCommand;
 use Spatie\Health\Commands\RunHealthChecksCommand;
 
 class Kernel extends ConsoleKernel
@@ -17,13 +24,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('p3d:skinuserupdate')->hourlyAt(10);
-        $schedule->command('server:pingall')->hourly();
-        $schedule->command('gj:update-trophies')->hourly();
-        $schedule->command('github:syncrelease')->dailyAt('00:00');
-        $schedule->command('discord:syncroles')->dailyAt('12:00');
-        $schedule->command('discord:syncuserroles')->dailyAt('12:10');
-        $schedule->command('activity:cleanup')->dailyAt('01:00');
+        $schedule->command(SkinUserUpdate::class)->hourlyAt(10);
+        $schedule->command(PingAllServers::class)->hourly();
+        $schedule->command(UpdateGamejoltAccountTrophies::class)->hourly();
+        $schedule->command(SyncGameVersion::class)->dailyAt('00:00');
+        $schedule->command(DiscordRoleSync::class)->dailyAt('12:00');
+        $schedule->command(DiscordUserRoleSync::class)->dailyAt('12:10');
+        $schedule->command(CleanActivitylogCommand::class)->dailyAt('01:00');
         $schedule->command(RunHealthChecksCommand::class)->everyMinute();
         $schedule->command(NotifyGameUpdate::class)->dailyAt('00:30');
     }

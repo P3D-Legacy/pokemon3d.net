514e6d60e736fe43ae6bb3f3101976ec5c15a024
Daniel S. Billing <daniel@kilobyte.no>
2022-05-22 14:04:26 +0200

    Update tailwind.blade.php

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -2,7 +2,7 @@
     <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex items-center justify-between">
         <div class="flex justify-between flex-1 sm:hidden">
             @if ($paginator->onFirstPage())
-                <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-700 cursor-default leading-5 rounded-md">
+                <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-700 select-none leading-5 rounded-md">
                     {!! __('pagination.previous') !!}
                 </span>
             @else
@@ -16,7 +16,7 @@
                     {!! __('pagination.next') !!}
                 </a>
             @else
-                <span class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-500 bg-white dark:bg-gray-900 border border-gray-300 cursor-default leading-5 rounded-md">
+                <span class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-500 bg-white dark:bg-gray-900 border border-gray-300 select-none leading-5 rounded-md">
                     {!! __('pagination.next') !!}
                 </span>
             @endif
@@ -59,7 +59,7 @@
                         {{-- "Three Dots" Separator --}}
                         @if (is_string($element))
                             <span aria-disabled="true">
-                                <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white  dark:bg-gray-800 border border-gray-300  dark:border-gray-700 cursor-default leading-5">{{ $element }}</span>
+                                <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 select-none leading-5">{{ $element }}</span>
                             </span>
                         @endif
 

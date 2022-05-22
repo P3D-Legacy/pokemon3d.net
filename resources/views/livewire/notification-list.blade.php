28f5818625936c6ac7f32895d0f940309cac3b39
Daniel S. Billing <daniel@kilobyte.no>
2022-05-22 13:15:13 +0200

    Use custom pagination

--- resources/views/livewire/notification-list.blade.php
+++ resources/views/livewire/notification-list.blade.php
@@ -38,6 +38,6 @@
                 @endforelse
             </div>
         </div>
-        {{ $notifications->links() }}
+        {{ $notifications->links('vendor.pagination.tailwind') }}
     </div>
 </div>

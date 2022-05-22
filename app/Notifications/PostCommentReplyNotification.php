033d814673c94799223afcbbde325af6b6b22063
Daniel S. Billing <daniel@kilobyte.no>
2022-05-22 13:20:28 +0200

    Added link to post in notification

--- app/Notifications/PostCommentReplyNotification.php
+++ app/Notifications/PostCommentReplyNotification.php
@@ -30,7 +30,10 @@ class PostCommentReplyNotification extends Notification
                 '">' .
                 $this->comment->creator->username .
                 '</a>',
-            'title' => "'" . $this->comment->commentable->title . "'",
+            'title' => '<a class="text-green-400 no-underline hover:underline" href="' .
+                route('blog.show', $this->comment->commentable->uuid) .
+                '">' . $this->comment->commentable->title .
+                '</a>',
         ]);
         $this->icon =
             '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" /></svg>';

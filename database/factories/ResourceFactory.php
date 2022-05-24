5b0b3b76c2dee60ef65454fc6ed3ff89a79865f7
Daniel S. Billing <daniel@kilobyte.no>
2022-05-24 19:01:35 +0200

    Create ResourceFactory.php

---
+++ database/factories/ResourceFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Resource>
+ */
+class ResourceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \App\Models\Resource::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'brief' => $this->faker->optional()->sentence,
+            'description' => $this->faker->optional()->paragraph,
+            'user_id' => UserFactory::new(),
+        ];
+    }
+}

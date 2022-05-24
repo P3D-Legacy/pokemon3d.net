d9d163810dcdcc4afb39b85e5548d44c0a4fef1f
DanielRTRD <DanielRTRD@users.noreply.github.com>
2022-05-24 19:26:19 +0000

    Prettified Code!

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,7 +31,7 @@ class UserFactory extends Factory
             'username' => $this->faker->unique()->userName(),
             'about' => $this->faker->optional()->sentence(),
             'gender' => $this->faker->numberBetween(0, 3),
-            'location' => $this->faker->city() .', '. $this->faker->country(),
+            'location' => $this->faker->city() . ', ' . $this->faker->country(),
             'birthdate' => $this->faker->date($max = Carbon::now()->subYears(13)),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password

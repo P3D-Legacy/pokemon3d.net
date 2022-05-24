d15e1a4024a28cac4cd52d0218844fafc5e9d261
Daniel S. Billing <daniel@kilobyte.no>
2022-05-24 17:48:02 +0200

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Team;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Features;
@@ -20,9 +21,17 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'username' => $this->faker->unique()->userName,
+            'about' => $this->faker->optional()->sentence(),
+            'gender' => $this->faker->numberBetween(0, 3),
+            'location' => $this->faker->city,
+            'birthdate' => $this->faker->date($max = Carbon::now()->subYears(13)),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'timezone' => $this->faker->timezone,
+            'locale' => $this->faker->locale,
+            'last_active_at' => $this->faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now'),
         ];
     }
 

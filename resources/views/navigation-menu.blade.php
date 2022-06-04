3d76190e2085b4d645bd2bf3e86dcc318b8c3946
Daniel S. Billing <daniel@kilobyte.no>
2022-06-04 19:41:06 +0200

    Changed text in menu for mod panel

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -156,7 +156,7 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                         </svg>
-                                        {{ __('Admin') }}
+                                        {{ __('Mod Panel') }}
                                     </button>
                                 </span>
                             </x-slot>
@@ -321,7 +321,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                         </svg>
-                        {{ __('Admin') }}
+                        {{ __('Mod Panel') }}
                     </div>
                 </div>
                 <div class="mt-3 space-y-1">

3d800adaa7f02b7712c941dd9b46e84c505c198b
Daniel S. Billing <daniel@izy.as>
2022-07-02 18:01:08 +0200

    Update NovaServiceProvider.php

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -80,9 +80,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            return $user->can('admin');
         });
     }
 

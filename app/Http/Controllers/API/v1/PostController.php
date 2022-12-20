5e4d55a9d47d5089ef051cbf1c0715eb27c08ebf
Daniel S. Billing <daniel@izy.no>
2022-12-20 11:13:02 +0100

    Pint

--- app/Http/Controllers/API/v1/PostController.php
+++ app/Http/Controllers/API/v1/PostController.php
@@ -56,7 +56,7 @@ class PostController extends Controller
             'user_id' => 'required|integer',
         ]);
         $post = Post::create($request->all());
+
         return response()->json($post, 201);
     }
-
 }

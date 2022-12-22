223b3c68b479fd5949fa2aa9c621e1f88d1ddfc4
Daniel S. Billing <daniel@izy.no>
2022-12-22 18:15:37 +0100

    Update GamejoltAccountBanController.php

--- app/Http/Controllers/API/v1/GamejoltAccountBanController.php
+++ app/Http/Controllers/API/v1/GamejoltAccountBanController.php
@@ -144,7 +144,8 @@ class GamejoltAccountBanController extends Controller
     /**
      * Remove the specified resource.
      *
-     * @urlParam id string required The UUID of the _ban_ you would like to remove.
+     * @urlParam id string required The UUID of the _ban_ you would like to remove
+     *
      * @response 202 {
      *  "success": 'Ban was removed!',
      * }

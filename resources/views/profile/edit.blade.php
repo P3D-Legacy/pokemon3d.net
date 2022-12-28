729558a44da57b76fb622a11d7990d9cbee58cf1
Daniel S. Billing <daniel@izy.no>
2022-12-28 20:33:10 +0100

    Update edit.blade.php

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -9,7 +9,6 @@
         <div class="py-10 mx-auto max-w-7xl sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
                 @livewire('profile.update-profile-information-form')
-
                 <x-jet-section-border />
             @endif
 

fa36615758d11aba2aa561b8aa4f110a10e60314
Daniel S. Billing <daniel@izy.no>
2022-12-29 15:58:12 +0100

    Update 2022_12_28_200859_create_game_saves_table.php

--- database/migrations/2022_12_28_200859_create_game_saves_table.php
+++ database/migrations/2022_12_28_200859_create_game_saves_table.php
@@ -15,23 +15,23 @@ return new class extends Migration
     {
         Schema::create('game_saves', function (Blueprint $table) {
             $table->uuid();
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->string('apricorns');
-            $table->string('berries');
-            $table->string('box');
-            $table->string('daycare');
-            $table->string('halloffame');
-            $table->string('itemdata');
-            $table->string('items');
-            $table->string('npc');
-            $table->string('options');
-            $table->string('party');
-            $table->string('player');
-            $table->string('pokedex');
-            $table->string('register');
-            $table->string('roamingpokemon');
-            $table->string('secretbase');
-            $table->string('statistics');
+            $table->bigInteger('user_id');
+            $table->longText('apricorns');
+            $table->longText('berries');
+            $table->longText('box');
+            $table->longText('daycare');
+            $table->longText('halloffame');
+            $table->longText('itemdata');
+            $table->longText('items');
+            $table->longText('npc');
+            $table->longText('options');
+            $table->longText('party');
+            $table->longText('player');
+            $table->longText('pokedex');
+            $table->longText('register');
+            $table->longText('roamingpokemon');
+            $table->longText('secretbase');
+            $table->longText('statistics');
             $table->timestamps();
         });
     }

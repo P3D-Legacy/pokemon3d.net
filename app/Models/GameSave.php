391365b86dc19db24879cd4322449f6325ceda09
Daniel S. Billing <daniel@izy.no>
2022-12-29 15:59:07 +0100

    Update GameSave.php

--- app/Models/GameSave.php
+++ app/Models/GameSave.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class GameSave extends Model
 {
@@ -28,6 +29,37 @@ class GameSave extends Model
         'secretbase',
         'statistics',
     ];
+
+    protected $primaryKey = 'uuid';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            $model->uuid = Str::uuid()->toString();
+        });
+
+        self::updating(function ($model) {
+            if (! $model->uuid) {
+                $model->uuid = Str::uuid()->toString();
+            }
+        });
+    }
 
     public function user()
     {

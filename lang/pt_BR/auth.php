9131d7d0ff357f3a28f8c9630665077b0e61a032
Daniel S. Billing <daniel@izy.no>
2023-01-08 20:03:23 +0100

    Added support for pt-BR language

---
+++ lang/pt_BR/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Credenciais informadas não correspondem com nossos registros.',
+    'password' => 'A senha informada está incorreta.',
+    'throttle' => 'Você realizou muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+];

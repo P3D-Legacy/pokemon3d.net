76bac0299f2cf31dc4871a4b9d618746bd38744f
Daniel S. Billing <daniel@izy.no>
2023-01-24 22:56:42 +0100

    Added info alert for searching for a user

--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -11,6 +11,16 @@
                 ['url' => null, 'label' => __('Members')],
             ]])
             @endcomponent
+
+            <div class="flex p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
+                <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Info</span>
+                <div>
+                    <p class="font-bold">Want to search for a user?</p>
+                    Use the search icon in the menu bar or click <kbd class="px-1.5 py-1 mx-0.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-md dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">Ctrl</kbd> + <kbd class="px-1.5 py-1 mx-0.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-md dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">K</kbd> or <kbd class="px-1.5 py-1 mx-0.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-md dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">CMD</kbd> + <kbd class="px-1.5 py-1 mx-0.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-md dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">K</kbd> to open it, then type "user" and press enter, then type the user you want to search for.
+                </div>
+            </div>
+
             <div class="flex flex-col items-center justify-center w-full mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-900 dark:shadow-gray-700">
                 <div class="flex flex-col w-full divide-y divide dark:divide-gray-700">
                     @forelse($users as $user)

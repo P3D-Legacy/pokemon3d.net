a0e22283d5381fe39ec2664a270faa091b3f8a78
Daniel S. Billing <daniel@izy.no>
2023-01-24 22:43:39 +0100

    Renamed mod menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -159,7 +159,7 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                         </svg>
-                                        {{ __('Moderation') }}
+                                        {{ __('Mod') }}
                                     </button>
                                 </span>
                             </x-slot>

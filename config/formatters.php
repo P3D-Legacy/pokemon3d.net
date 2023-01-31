609709386d32a6e0f7e8174f7b21f58cf58ba2d5
Daniel S. Billing <daniel@kilobyte.no>
2023-01-31 22:11:07 +0100

    Installed michael-rubel/laravel-formatters

---
+++ config/formatters.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Formatters Folder
+    |--------------------------------------------------------------------------
+    |
+    | You can change the folder you want to use for your formatters.
+    |
+    */
+
+    'folder' => 'app' . DIRECTORY_SEPARATOR . 'Formatters',
+
+    /*
+    |--------------------------------------------------------------------------
+    | String Bindings Case
+    |--------------------------------------------------------------------------
+    |
+    | The string convention of registered container bindings.
+    |
+    | Options: 'snake', 'kebab'
+    | Default: 'kebab'
+    |
+    */
+
+    'bindings_case' => 'kebab',
+
+];

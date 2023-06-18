4d49c1bf0410052da47fd4e6ba9f382a7d4d1316
Daniel S. Billing <daniel@izy.no>
2023-06-18 16:24:31 +0200

    Run all endpoints

--- config/scribe.php
+++ config/scribe.php
@@ -87,7 +87,7 @@ return [
                      * API calls will be made only for routes in this group matching these HTTP methods (GET, POST, etc).
                      * List the methods here or use '*' to mean all methods. Leave empty to disable API calls.
                      */
-                    'methods' => ['GET'],
+                    'methods' => ['*'],
 
                     /*
                      * Laravel config variables which should be set for the API call.

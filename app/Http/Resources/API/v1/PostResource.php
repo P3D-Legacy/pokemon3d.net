5a145c4af75b8fcff8bdaee2551fb6332825fbe6
Daniel S. Billing <daniel@izy.no>
2023-06-18 16:01:07 +0200

    Create PostResource.php

---
+++ app/Http/Resources/API/v1/PostResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\API\v1;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request): array|JsonSerializable|Arrayable
+    {
+        return parent::toArray($request);
+    }
+}

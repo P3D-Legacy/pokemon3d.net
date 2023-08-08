f8d598e058102bfb77c4c36a24fbad628396cfb9
Daniel S. Billing <daniel@izy.no>
2023-08-08 21:13:29 +0200

    Refactor server list

--- app/Http/Livewire/Server/ServerCardList.php
+++ app/Http/Livewire/Server/ServerCardList.php
@@ -8,6 +8,8 @@ use Livewire\Component;
 class ServerCardList extends Component
 {
     public $servers;
+    public $my_servers;
+    public $user;
 
     protected $listeners = [
         'serverUpdated' => 'update',
@@ -15,22 +17,35 @@ class ServerCardList extends Component
 
     public function mount()
     {
-        $this->servers = Server::orderBy('official', 'desc')
-            ->orderBy('last_online_at', 'desc')
-            ->orderBy('ping', 'asc')
-            ->get();
+        $this->loadData();
     }
 
     public function update()
     {
-        $this->servers = Server::orderBy('official', 'desc')
-            ->orderBy('last_online_at', 'desc')
-            ->orderBy('ping', 'asc')
-            ->get();
+        $this->loadData();
     }
 
     public function render()
     {
         return view('livewire.server.server-card-list');
+    }
+
+    /**
+     * @return void
+     */
+    private function loadData(): void
+    {
+        $this->user = auth()->user() ?? null;
+        $this->servers = isset($this->user) ? Server::where('user_id', '!=', $this->user->id)
+            ->where('active', true)
+            ->orderBy('official', 'desc')
+            ->orderBy('last_online_at', 'desc')
+            ->orderBy('ping', 'asc')
+            ->get() : Server::orderBy('official', 'desc')
+            ->where('active', true)
+            ->orderBy('last_online_at', 'desc')
+            ->orderBy('ping', 'asc')
+            ->get();
+        $this->my_servers = $this->user->id ? Server::where('user_id', $this->user->id)->get() : [];
     }
 }
--- resources/views/livewire/server/server-card-list.blade.php
+++ resources/views/livewire/server/server-card-list.blade.php
@@ -1,10 +1,17 @@
 <div class="flex flex-col">
-    <div class="-my-2 sm:-mx-6 lg:-mx-8">
-        <p class="pt-4 pb-1 text-center text-gray-300 dark:text-gray-600">{{ __('All server statuses are updated every hour.') }}</p>
-        <div class="grid grid-cols-1 gap-4 sm:p-10 sm:grid-cols-2 md:grid-cols-3 auto-rows-auto grid-flow-rows">
-            @foreach ($this->servers as $server)
+    <p class="pt-4 pb-1 text-center text-gray-300 dark:text-gray-600">{{ __('All server statuses are updated every hour.') }}</p>
+    @if($this->my_servers->count() > 0)
+        <h1 class="text-xl text-gray-600 dark:text-gray-300">{{ __('Your servers') }}:</h1>
+        <div class="grid grid-cols-1 gap-4 sm:p-5 sm:grid-cols-2 md:grid-cols-3 auto-rows-auto grid-flow-rows">
+            @foreach($this->my_servers as $server)
                 @livewire('server.server-card', ['server' => $server])
             @endforeach
         </div>
+        <div class="border-t my-8 border-dotted border-gray-300 dark:border-gray-600"></div>
+    @endif
+    <div class="grid grid-cols-1 gap-4 sm:p-5 sm:grid-cols-2 md:grid-cols-3 auto-rows-auto grid-flow-rows">
+        @foreach($this->servers as $server)
+            @livewire('server.server-card', ['server' => $server])
+        @endforeach
     </div>
 </div>

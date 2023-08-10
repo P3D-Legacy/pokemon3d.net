ec9c76d2cde16eb7c459925633343a4ea1e66076
Daniel S. Billing <daniel@izy.no>
2023-08-10 15:44:33 +0200

    Update livewire.php

--- config/livewire.php
+++ config/livewire.php
@@ -2,6 +2,6 @@
 
 return [
     'temporary_file_upload' => [
-        'rules' => 'file|max:100000', // (100MB max, and only pngs, jpegs, and pdfs.)
+        'rules' => 'file|max:102400', // 100MB
     ],
 ];

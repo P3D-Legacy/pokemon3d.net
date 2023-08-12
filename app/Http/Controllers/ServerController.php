b44b674dbacdd4e49d698d6ac4c1892050a97cf0
Daniel S. Billing <daniel@izy.no>
2023-08-12 11:35:54 +0200

    Refactored ServerController

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Server;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ServerController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware(['auth', 'verified'])->only(['edit', 'update', 'destroy']);
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
@@ -20,7 +30,7 @@ class ServerController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -30,7 +40,7 @@ class ServerController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -41,7 +51,7 @@ class ServerController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show($id)
     {
@@ -51,12 +61,11 @@ class ServerController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Server $server
+     * @return Application|Factory|View
      */
     public function edit(Server $server)
     {
-        abort_if(! $server, 404);
         abort_if(! $server->user_id == auth()->user()->id, 403);
 
         return view('server.edit', compact('server'));
@@ -66,7 +75,7 @@ class ServerController extends Controller
      * Update the specified resource in storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -77,7 +86,7 @@ class ServerController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {

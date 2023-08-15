a45af6531992a7e86415f8e3920b454cd986a42a
Daniel S. Billing <daniel@izy.no>
2023-08-15 22:02:13 +0200

    Removed font awesome icon, replaced with heroicon

--- resources/views/skin/index.blade.php
+++ resources/views/skin/index.blade.php
@@ -20,7 +20,10 @@
                                         <p class="dark:text-slate-400">@lang('No skins have been added to your account yet.')</p>
                                         @if(Auth::user()->gamejolt->skins()->count() >= env('SKIN_MAX_UPLOAD'))
                                             <div class="px-4 py-3 mt-4 text-sm leading-normal text-blue-800 bg-blue-300 rounded-lg">
-                                                <i class="mr-2 fas fa-info-circle" aria-hidden="true"></i> @lang('Your slots are full. You cannot import from the old site unless you delete one of the slots.')
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1 inline-block">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                                                </svg>
+                                                @lang('Your slots are full. You cannot import from the old site unless you delete one of the slots.')
                                             </div>
                                         @else
                                             <p class="mt-3 text-sm">

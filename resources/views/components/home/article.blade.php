c133ace51e37c2db60b8be41c733379ae32003f1
Daniel S. Billing <daniel@izy.no>
2023-08-16 20:18:45 +0200

    Minor change to article

--- resources/views/components/home/article.blade.php
+++ resources/views/components/home/article.blade.php
@@ -38,19 +38,19 @@
         </p>
     </div>
 
-    <dl class="mt-6 flex gap-4 xl:gap-6 bottom-0 absolute pb-6">
+    <dl class="mt-6 flex gap-3 xl:gap-6 bottom-0 absolute pb-6">
         <div class="flex flex-col-reverse">
-            <dt class="text-sm font-medium text-slate-600 dark:text-slate-400 hidden sm:block">{{ __('Published') }}</dt>
+            <dt class="text-xs font-medium text-slate-600 dark:text-slate-400 hidden sm:block">{{ __('Published') }}</dt>
             <dd class="text-xs text-slate-500 dark:text-slate-300">{{ now()->subYear(1) > $post->published_at ? $post->published_at->isoFormat('LL') : $post->published_at->diffForHumans() }}</dd>
         </div>
 
         <div class="flex flex-col-reverse">
-            <dt class="text-sm font-medium text-slate-600 dark:text-slate-400 hidden sm:block">{{ __('Reading time') }}</dt>
+            <dt class="text-xs font-medium text-slate-600 dark:text-slate-400 hidden sm:block">{{ __('Reading time') }}</dt>
             <dd class="text-xs text-slate-500 dark:text-slate-300">{{ read_time($post->body) }}</dd>
         </div>
 
         <div class="flex flex-col-reverse">
-            <dt class="text-sm font-medium text-slate-600 dark:text-slate-400 hidden sm:block">{{ __('Comments') }}</dt>
+            <dt class="text-xs font-medium text-slate-600 dark:text-slate-400 hidden sm:block">{{ __('Comments') }}</dt>
             <dd class="text-xs text-slate-500 dark:text-slate-300">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 inline-block sm:hidden">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
@@ -60,7 +60,7 @@
         </div>
 
         <div class="flex flex-col-reverse">
-            <dt class="text-sm font-medium text-slate-600 dark:text-slate-400 hidden sm:block">{{ __('Views') }}</dt>
+            <dt class="text-xs font-medium text-slate-600 dark:text-slate-400 hidden sm:block">{{ __('Views') }}</dt>
             <dd class="text-xs text-slate-500 dark:text-slate-300">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 inline-block sm:hidden">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />

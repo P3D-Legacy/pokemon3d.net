9dc2f5f664951db6ff33c3f281a624fe36d81c9e
Daniel S. Billing <daniel@izy.no>
2023-08-28 16:00:23 +0200

    Fixed wire-elements modal styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,14 @@ module.exports = {
             position: ['top', 'right'],
         },
     },
+
+    safelist: [
+        {
+          pattern: /max-w-(sm|md|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl)/,
+          variants: ['sm', 'md', 'lg', 'xl', '2xl'],
+        },
+    ],
+
     plugins: [
         require('@tailwindcss/forms')({
             strategy: 'class',

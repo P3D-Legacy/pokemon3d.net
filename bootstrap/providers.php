2b7df90aaf75c847e56132dc5cba2283d7af5ff5
Daniel S. Billing <daniel@kilobyte.no>
2025-04-08 22:29:57 +0200

    Remove deprecated provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\NovaServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,

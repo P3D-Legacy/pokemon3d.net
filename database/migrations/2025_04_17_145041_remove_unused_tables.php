84521ff92be118b6ac28c1dddeb1c0d0e86e0be3
Daniel S. Billing <daniel@kilobyte.no>
2025-08-30 10:58:34 +0200

    Create 2025_04_17_145041_remove_unused_tables.php

---
+++ database/migrations/2025_04_17_145041_remove_unused_tables.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('nova_field_attachments');
+        Schema::dropIfExists('nova_notifications');
+        Schema::dropIfExists('nova_pending_field_attachments');
+        Schema::dropIfExists('twitter_accounts');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};

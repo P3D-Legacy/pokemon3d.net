1c07b46417affa4f91fbb1f72f34df06017bba78
Daniel S. Billing <daniel@kilobyte.no>
2025-08-30 11:00:26 +0200

    Switch file storage to AWS S3 and update config
    Added league/flysystem-aws-s3-v3 and related AWS dependencies to composer. Updated filesystems.php to use S3 for 'player' and 'skin' disks, and added a default 's3' disk configuration. Changed Jetstream's profile photo disk to use S3 instead of 'public'.

--- config/filesystems.php
+++ config/filesystems.php
@@ -3,14 +3,46 @@
 return [
 
     'disks' => [
+
+        's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
         'player' => [
-            'driver' => 'local',
-            'root' => public_path().'/player',
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'visibility' => 'public',
+            'throw' => false,
+            'root' => 'player',
         ],
 
         'skin' => [
-            'driver' => 'local',
-            'root' => public_path().'/img/skin',
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'visibility' => 'public',
+            'throw' => false,
+            'root' => 'skin',
         ],
     ],
 
